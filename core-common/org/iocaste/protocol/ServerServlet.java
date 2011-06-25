0d67604f1d4de342a3eceeca7000a6cb1a88bfb2
Francisco Prates <mad.kanie@gmail.com>
2011-06-25 20:03:14 -0300

    core-common: IocasteServlet tinha apenas um usuario. Passa a integrar ServerServlet.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- core-common/org/iocaste/protocol/ServerServlet.java
+++ core-common/org/iocaste/protocol/ServerServlet.java
@@ -1,24 +1,74 @@
 package org.iocaste.protocol;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.hibernate.SessionFactory;
 
-public abstract class ServerServlet extends IocasteServlet {
+public abstract class ServerServlet extends HttpServlet {
     private static final long serialVersionUID = 7408336035974886402L;
     private SessionFactory sessionFactory;
     private Map<String, Function> functions;
+    private HttpServletRequest req;
+    private HttpServletResponse resp;
     
     public ServerServlet() {
         functions = new HashMap<String, Function>();
         sessionFactory = HibernateListener.getSessionFactory();
         config();
     }
+    
+    /*
+     * 
+     * Getters
+     * 
+     */
+    
+    private final String getUrl() {
+        String url = new StringBuffer(req.getScheme()).append("://")
+            .append(req.getServerName()).append(":")
+            .append(req.getServerPort())
+            .append(req.getContextPath())
+            .append(req.getServletPath()).toString();
+        
+        return url;
+    }
+    
+    /*
+     * 
+     * Others
+     * 
+     */
+    
+    @Override
+    protected final void doGet(
+            HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected final void doPost(
+            HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        this.req = req;
+        this.resp = resp;
+        
+        try {
+            entry();
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
 
     protected void preRun(Message message) throws Exception { }
     
-    @Override
     protected final void entry() throws Exception {
         Message message;
         Function function;
@@ -54,4 +104,13 @@ public abstract class ServerServlet extends IocasteServlet {
     }
     
     protected abstract void config();
+    
+    protected final Service serviceInstance() throws IOException {
+        return new Service(req.getSession().getId(), getUrl());
+    }
+    
+    protected final void configureStreams(Service service) throws IOException {
+        service.setInputStream(req.getInputStream());
+        service.setOutputStream(resp.getOutputStream());
+    }
 }
