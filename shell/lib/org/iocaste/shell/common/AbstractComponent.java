b420fb245e123e59e6a57f2bb595c1523819e271
Francisco Prates <mad.kanie@gmail.com>
2011-08-03 23:36:05 -0300

    shell: componentes guardam container de origem.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- shell/lib/org/iocaste/shell/common/AbstractComponent.java
+++ shell/lib/org/iocaste/shell/common/AbstractComponent.java
@@ -3,12 +3,24 @@ package org.iocaste.shell.common;
 public abstract class AbstractComponent extends AbstractElement implements Component {
     private static final long serialVersionUID = -5327168368336946819L;
     private String name;
+    private Container container;
     
     public AbstractComponent(Container container, Const type) {
         super(type);
         
+        this.container = container;
+        
         if (container != null)
             container.add(this);
+    }
+    
+    /*
+     * (non-Javadoc)
+     * @see org.iocaste.shell.common.Component#getContainer()
+     */
+    @Override
+    public final Container getContainer() {
+        return container;
     }
     
     /*
--- shell/lib/org/iocaste/shell/common/Component.java
+++ shell/lib/org/iocaste/shell/common/Component.java
@@ -2,6 +2,8 @@ package org.iocaste.shell.common;
 
 public interface Component extends Element {
 
+    public abstract Container getContainer();
+    
     public abstract String getName();
     
     public abstract void setName(String name);
