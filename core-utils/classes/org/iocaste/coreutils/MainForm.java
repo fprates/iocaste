670b8bd24c47a939f220c0ebed7533ce64e8e6fd
Francisco Prates <mad.kanie@gmail.com>
2011-08-04 00:17:25 -0300

    core-utils: implementa buildViews().
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- core-utils/classes/org/iocaste/coreutils/MainForm.java
+++ core-utils/classes/org/iocaste/coreutils/MainForm.java
@@ -1,17 +1,33 @@
 package org.iocaste.coreutils;
 
 import org.iocaste.shell.common.AbstractForm;
+import org.iocaste.shell.common.Form;
+import org.iocaste.shell.common.Menu;
+import org.iocaste.shell.common.MenuItem;
 import org.iocaste.shell.common.ViewData;
 
 public class MainForm extends AbstractForm {
 
-    public MainForm() {
-        ViewData view = new ViewData();
-        // inicialize os componente da tela aqui
-        addView("main", view);
+    /*
+     * (non-Javadoc)
+     * @see org.iocaste.shell.common.AbstractForm#buildViews()
+     */
+    @Override
+    protected final void buildViews() {
+        Form form;
+        ViewData view = getViewInstance("main");
+        
+        Menu mainMenu = new Menu(null, "run");
+        new MenuItem(mainMenu, "user.add", "add");
+        new MenuItem(mainMenu, "user.change", "change");
+        view.setContainer(mainMenu);
+        
+        view = getViewInstance("form");
+        form = new Form(null, "user");
+        view.setContainer(form);
     }
     
     public final void submit() {
-        // implemente ações como nesse exemplo
+        
     }
 }
