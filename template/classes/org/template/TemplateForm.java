5581897543b447c477053d11ada52e13438e7857
Francisco Prates <mad.kanie@gmail.com>
2011-08-06 18:51:07 -0300

    template: reflete alteraçao na chamada da visao/controlador.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- template/classes/org/template/TemplateForm.java
+++ template/classes/org/template/TemplateForm.java
@@ -1,23 +1,22 @@
 package org.template;
 
 import org.iocaste.shell.common.AbstractForm;
+import org.iocaste.shell.common.ControlData;
 import org.iocaste.shell.common.StandardContainer;
 import org.iocaste.shell.common.Text;
 import org.iocaste.shell.common.ViewData;
 
 public class TemplateForm extends AbstractForm {
 
-    @Override
-    protected void buildViews() {
-        ViewData view = getViewInstance("main");
-        StandardContainer container = new StandardContainer(null);
-        Text text = new Text(container);
+    public void main(ViewData view) {
+        StandardContainer container = new StandardContainer(null, null);
+        Text text = new Text(container, null);
         
         text.setText("Aplicação Iocaste Template");
         view.setContainer(container);
     }
     
-    public final void templateaction() {
+    public final void templateaction(ControlData controldata, ViewData view) {
         // implemente ações como nesse exemplo
     }
 }
