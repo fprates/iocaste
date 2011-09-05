51b5e070669f309dfff2e2dc7d8704722056e79c
Francisco Prates <mad.kanie@gmail.com>
2011-09-04 22:58:30 -0300

    core-utils: implementação inicial de upload de arquivos.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- core-utils/classes/org/iocaste/coreutils/MainForm.java
+++ core-utils/classes/org/iocaste/coreutils/MainForm.java
@@ -5,15 +5,18 @@ import org.iocaste.documents.common.Documents;
 import org.iocaste.protocol.Iocaste;
 import org.iocaste.protocol.user.User;
 import org.iocaste.shell.common.AbstractPage;
+import org.iocaste.shell.common.Button;
 import org.iocaste.shell.common.Const;
 import org.iocaste.shell.common.Container;
 import org.iocaste.shell.common.ControlData;
 import org.iocaste.shell.common.DataForm;
+import org.iocaste.shell.common.FileEntry;
 import org.iocaste.shell.common.Form;
 import org.iocaste.shell.common.Menu;
 import org.iocaste.shell.common.MenuItem;
 import org.iocaste.shell.common.MessageSource;
 import org.iocaste.shell.common.ViewData;
+
 
 public class MainForm extends AbstractPage {
 
@@ -25,9 +28,11 @@ public class MainForm extends AbstractPage {
     public final void main(ViewData view) throws Exception {
         Container form = new Form(null, "main");
         Menu mainMenu = new Menu(form, "run");
-        
+
+        new MenuItem(mainMenu, "data.upload", "upload");
         new MenuItem(mainMenu, "user.add", "add");
         new MenuItem(mainMenu, "user.change", "change");
+        
         view.setMessages(new MessageSource("/messages.properties"));
         view.addContainer(form);
     }
@@ -59,6 +64,9 @@ public class MainForm extends AbstractPage {
         Menu mainmenu= (Menu)view.getElement("run");
         String value = mainmenu.getParameter().getValue();
         
+        if (value.equals("upload"))
+            controldata.redirect("iocaste-core-utils", "uploadform");
+        
         if (value.equals("add"))
             controldata.redirect("iocaste-core-utils", "form");
     }
@@ -80,5 +88,32 @@ public class MainForm extends AbstractPage {
         } catch (Exception e) {
             controldata.message(Const.ERROR, "user.already.exists");
         }
+    }
+    
+    /**
+     * 
+     * @param cdata
+     * @param vdata
+     */
+    public final void upload(ControlData cdata, ViewData vdata) {
+        System.out.println(((FileEntry)vdata.getElement("file")).getValue());
+    }
+    
+    /**
+     * 
+     * @param view
+     */
+    public final void uploadform(ViewData view) {
+        Button upload;
+        Form form = new Form(null, "fileupload");
+        FileEntry file = new FileEntry(form, "file");
+        
+        upload = new Button(form, "upload");
+        file.setDestiny("/home/francisco.prates/Download/apache-tomcat/webapps/iocaste-core-utils/WEB-INF/data/");
+        form.setEnctype("multipart/form-data");
+        upload.setSubmit(true);
+        
+        view.addContainer(form);
+        view.setTitle("file-upload");
     }
 }
