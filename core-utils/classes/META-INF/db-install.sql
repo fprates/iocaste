fa9f3bdf6bebd7e9278fc6b60ebf59e254123e7f
Francisco Prates <mad.kanie@gmail.com>
2011-09-07 15:05:20 -0300

    common: dados de configuraçao, exceto hibernate.cfg.xml, devem ficar em /META-INF.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- core-utils/classes/META-INF/db-install.sql
+++ core-utils/classes/META-INF/db-install.sql
@@ -1,7 +1,1 @@
-insert into docs001 (docid) values ('LOGIN');
-
-insert into docs002 (iname, docid) values ('USERNAME', 'LOGIN');
-insert into docs002 (iname, docid) values ('SECRET', 'LOGIN');
-
-commit work;
 
---
+++ login/classes/META-INF/db-install.sql
@@ -0,0 +1,9 @@
+\p running login script.
+insert into docs001 (docid) values ('LOGIN');
+
+insert into docs002 (iname, docid) values ('USERNAME', 'LOGIN');
+insert into docs002 (iname, docid) values ('SECRET', 'LOGIN');
+\p initial configuration saved.
+
+commit work;
+
