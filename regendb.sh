5aa58ca0d23d87f19a06fb7e170dbd1ee4cba0ee
Francisco Prates <mad.kanie@gmail.com>
2011-09-17 01:48:47 -0300

    build: correçao nos locais dos scripts de geraçao de tabelas.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- regendb.sh
+++ regendb.sh
@@ -2,9 +2,9 @@
 FROM=$(pwd)
 cd hsqldb/bin
 SQLTOOL="java -Dsqlfile.charset=UTF-8 -jar ../lib/sqltool.jar"
-DB_PATH=apache-tomcat/webapps
+DB_PATH=../../apache-tomcat/webapps
 DB_INSTALL=WEB-INF/classes/META-INF/db-install.sql
-$SQLTOOL localhost-sa $DB_PATH/iocaste-core/$DB_INSTALL
+$SQLTOOL localhost-sa $DB_PATH/iocaste-server/$DB_INSTALL
 $SQLTOOL localhost-sa $DB_PATH/iocaste-documents/$DB_INSTALL
 $SQLTOOL localhost-sa $DB_PATH/iocaste-shell/$DB_INSTALL
 $SQLTOOL localhost-sa $DB_PATH/iocaste-login/$DB_INSTALL
--- startup.sh
+++ startup.sh
@@ -2,13 +2,12 @@
 export JPDA_ADDRESS=8000
 export JPDA_TRANSPORT=dt_socket
 FROM=$(pwd)
-echo 'starting database manager...'
-cd ../hsqldb/bin/
+echo "starting database manager..."
+cd hsqldb/bin/
 java -Dsqlfile.charset=UTF-8 -cp ../lib/hsqldb.jar org.hsqldb.server.Server &
-echo 'done'
-echo 'starting servlet container...'
+echo "done"
+echo "starting servlet container..."
 cd ../../apache-tomcat/bin/
 ./catalina.sh jpda run
-echo 'done'
+echo "done"
 cd $FROM
-
