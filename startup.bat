72998f59221c4a9ff55f09ee2a365d935de13688
Francisco Prates <mad.kanie@gmail.com>
2011-09-16 23:48:10 -0300

    build: nao confia em scripts customizados para iniciar hsqldb e tomcat.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- startup.bat
+++ startup.bat
@@ -1,9 +1,11 @@
 @echo off
+set JPDA_ADDRESS=8000
+set JPDA_TRANSPORT=dt_socket
 echo "starting database manager..."
 cd ..\hsqldb\bin\
-call rundb-des.bat
+call java -Dsqlfile.charset=UTF-8 -cp ..\lib\hsqldb.jar org.hsqldb.server.Server
 echo "done"
 echo "starting servlet container..."
 cd ..\..\apache-tomcat\bin\
-call tomcat-start.bat
+call catalina.sh jpda run
 echo "done"
--- startup.sh
+++ startup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+export JPDA_ADDRESS=8000
+export JPDA_TRANSPORT=dt_socket
 FROM=$(pwd)
 echo 'starting database manager...'
 cd ../hsqldb/bin/
-./rundb-des.sh &
+java -Dsqlfile.charset=UTF-8 -cp ../lib/hsqldb.jar org.hsqldb.server.Server &
 echo 'done'
 echo 'starting servlet container...'
 cd ../../apache-tomcat/bin/
-./tomcat-start.sh
+./catalina.sh jpda run
 echo 'done'
 cd $FROM
 
