5d39e84c9bce837b7f2c2491bc6976132300e785
Francisco Prates <mad.kanie@gmail.com>
2011-11-09 18:01:18 -0200

    shell: substitui exportTo() por getObject(), que é baseado em ExtendedObject.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- shell/lib/org/iocaste/shell/common/DataForm.java
+++ shell/lib/org/iocaste/shell/common/DataForm.java
@@ -21,11 +21,10 @@
 
 package org.iocaste.shell.common;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.iocaste.documents.common.DocumentModelItem;
+import org.iocaste.documents.common.ExtendedObject;
 
 /**
  * Implementação de formulário de dados.
@@ -52,54 +51,6 @@ public class DataForm extends AbstractContainer {
     }
     
     /**
-     * Exporta dados do formulário para um objeto.
-     * @param object objeto
-     */
-    public final void exportTo(Object object) {
-        InputComponent item;
-        String formmethodname;
-        String objmethodname;
-        String name;
-        Method method_;
-        DocumentModelItem modelitem;
-        
-        for (Element element: getElements()) {
-            if (!element.isDataStorable())
-                continue;
-            
-            item = (InputComponent)element;
-            modelitem = item.getModelItem();
-
-            if (modelitem == null)
-                continue;
-            
-            name = modelitem.getName();
-            
-            formmethodname = new StringBuffer("set").
-                    append(name).toString().toLowerCase();
-            method_ = null;
-            
-            for (Method method : object.getClass().getMethods()) {
-                objmethodname = method.getName().toLowerCase();
-                
-                if (!objmethodname.equals(formmethodname))
-                    continue;
-                
-                method_ = method;
-                break;
-            }
-            
-            if (method_ == null)
-                continue;
-
-            for (Class<?> class_ : method_.getParameterTypes()) {
-                invokeCopy(class_, object, method_, item);
-                break;
-            }
-        }
-    }
-    
-    /**
      * Retorna ações associadas ao formulário.
      * @return ações
      */
@@ -109,61 +60,20 @@ public class DataForm extends AbstractContainer {
     
     /**
      * 
-     * @param class_
-     * @param object
-     * @param method
-     * @param component
-     * @throws RuntimeException
+     * @return
      */
-    private final void invokeCopy(Class<?> class_, Object object,
-            Method method, InputComponent component) throws RuntimeException {
-        String value = component.getValue();
-        String typename = class_.getSimpleName();
-        
-        try {
-            if (typename.equals("String")) {
-                method.invoke(object, value);
-                return;
-            }
-            
-            if (typename.equals("Integer")) {
-                method.invoke(object, Integer.parseInt(value));
-                return;
-            }
-            
-            if (typename.equals("Boolean")) {
-                method.invoke(object, Boolean.parseBoolean(value));
-                return;
-            }
-            
-            if (typename.equals("Character")) {
-                method.invoke(object, value.toCharArray()[0]);
-                return;
-            }
-            
-            if (typename.equals("Short")) {
-                method.invoke(object, Short.parseShort(value));
-                return;
-            }
-            
-            if (typename.equals("Float")) {
-                method.invoke(object, Float.parseFloat(value));
-                return;
-            }
-            
-            if (typename.equals("Double")) {
-                method.invoke(object, Double.parseDouble(value));
-                return;
-            }
-            
-            if (typename.equals("Byte")) {
-                method.invoke(object, Byte.parseByte(value));
-                return;
-            }
-        } catch (Exception e) {
-            value = new StringBuffer("Error loading parameter for ").
-                    append(method.getName()).append(".").toString();
-            throw new RuntimeException(value, e);
+    public final ExtendedObject getObject() {
+    	InputComponent input;
+    	ExtendedObject object = new ExtendedObject(getModel());
+    	
+        for (Element element: getElements()) {
+        	if (!element.isDataStorable())
+                continue;
+        	
+        	input = (InputComponent)element;
+        	object.setValue(input.getModelItem(), input.getValue());
         }
+    	
+    	return object;
     }
 }
