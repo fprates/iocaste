efedc09e8d05e1ff4d665dfdfa7ef67e297267da
Francisco Prates <mad.kanie@gmail.com>
2011-11-11 22:32:09 -0200

    shell: CheckBox nao e um controle, mas uma entrada.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- shell/lib/org/iocaste/shell/common/CheckBox.java
+++ shell/lib/org/iocaste/shell/common/CheckBox.java
@@ -1,9 +1,17 @@
 package org.iocaste.shell.common;
 
-public class CheckBox extends AbstractControlComponent {
+public class CheckBox extends AbstractInputComponent {
     private static final long serialVersionUID = -7042599709545616205L;
     
     public CheckBox(Container container, String name) {
-        super(container, Const.CHECKBOX, name);
+        super(container, Const.CHECKBOX, null, name);
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public final boolean isSelected() {
+        return Boolean.parseBoolean(getValue());
     }
 }
