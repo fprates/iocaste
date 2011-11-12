05ceb372cf8266ccf6aa98c82527720f41248367
Francisco Prates <mad.kanie@gmail.com>
2011-11-11 23:56:27 -0200

    documents: reimplementa contains() em DocumentModel.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- documents/lib/org/iocaste/documents/common/DocumentModel.java
+++ documents/lib/org/iocaste/documents/common/DocumentModel.java
@@ -82,7 +82,11 @@ public class DocumentModel implements Comparable<DocumentModel>, Serializable {
      * @return
      */
     public final boolean contains(DocumentModelItem item) {
-        return itens.contains(item);
+        for (DocumentModelItem modelitem : itens)
+            if (item.getName().equals(modelitem.getName()))
+                return true;
+        
+        return false;
     }
     
     /*
