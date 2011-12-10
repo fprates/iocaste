93e2211bdf3d140febbde3f843344e78553982d4
Francisco Prates <mad.kanie@gmail.com>
2011-12-10 01:27:54 -0200

    help: implementação inicial de utilitário de ajuda.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

---
+++ help/classes/org/iocaste/help/Main.java
@@ -0,0 +1,25 @@
+package org.iocaste.help;
+
+import org.iocaste.shell.common.AbstractPage;
+import org.iocaste.shell.common.Container;
+import org.iocaste.shell.common.Form;
+import org.iocaste.shell.common.Text;
+import org.iocaste.shell.common.ViewData;
+
+public class Main extends AbstractPage {
+
+    public void main(ViewData view) {
+        
+    }
+    
+    public final void view(ViewData vdata) {
+        Container form = new Form(null, "help.form");
+        Text help = new Text(form, "not.yet");
+        
+        help.setText("Help not implemented. Back soon later.");
+        
+        vdata.setTitle("help");
+        vdata.addContainer(form);
+        vdata.setNavbarActionEnabled("back", true);
+    }
+}
