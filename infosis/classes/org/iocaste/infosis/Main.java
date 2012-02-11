e9917b31a7f77a206e3940898742a5ac664fa3f0
Francisco Prates <mad.kanie@gmail.com>
2012-02-11 13:04:56 -0200

    infosis: sincroniza com alterações em Table.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- infosis/classes/org/iocaste/infosis/Main.java
+++ infosis/classes/org/iocaste/infosis/Main.java
@@ -6,6 +6,7 @@ import org.iocaste.shell.common.AbstractPage;
 import org.iocaste.shell.common.Container;
 import org.iocaste.shell.common.StandardContainer;
 import org.iocaste.shell.common.Table;
+import org.iocaste.shell.common.TableColumn;
 import org.iocaste.shell.common.TableItem;
 import org.iocaste.shell.common.Text;
 import org.iocaste.shell.common.ViewData;
@@ -19,8 +20,9 @@ public class Main extends AbstractPage {
         Container container = new StandardContainer(null, "container");
         Table table = new Table(container, "properties");
         
-        table.addColumn("name");
-        table.addColumn("value");
+        new TableColumn(table, "name");
+        new TableColumn(table, "value");
+        
         table.setMark(false);
         
         for (Object name : properties.keySet()) {
