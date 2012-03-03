c0d04fe41ca481ae924472331603f9dea70ca5fa
Francisco Prates <mad.kanie@gmail.com>
2012-03-03 00:19:32 -0300

    transport: constrói objetos a partir de instruções de um pool.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- transport/classes/org/iocaste/transport/Main.java
+++ transport/classes/org/iocaste/transport/Main.java
@@ -13,6 +13,7 @@ import org.iocaste.documents.common.DocumentModel;
 import org.iocaste.documents.common.DocumentModelItem;
 import org.iocaste.documents.common.DocumentModelKey;
 import org.iocaste.documents.common.Documents;
+import org.iocaste.protocol.Function;
 import org.iocaste.shell.common.AbstractPage;
 import org.iocaste.shell.common.Button;
 import org.iocaste.shell.common.Const;
@@ -21,191 +22,60 @@ import org.iocaste.shell.common.FileEntry;
 import org.iocaste.shell.common.Form;
 import org.iocaste.shell.common.Frame;
 import org.iocaste.shell.common.InputComponent;
+import org.iocaste.shell.common.Link;
 import org.iocaste.shell.common.SearchHelp;
 import org.iocaste.shell.common.Table;
 import org.iocaste.shell.common.TableColumn;
 import org.iocaste.shell.common.TableItem;
+import org.iocaste.shell.common.Text;
 import org.iocaste.shell.common.TextField;
 import org.iocaste.shell.common.ViewData;
 
 public class Main extends AbstractPage {
     private static final String IDTAG = "IOCASTE000001";
+    private static final int FILE_IS_EMPTY = 1;
+    private static final int INVALID_FILE = 2;
+    private static final int INVALID_HEADER = 3;
     
+    /**
+     * 
+     * @param view
+     * @throws Exception
+     */
     public final void add(ViewData view) throws Exception {
         Table objects = (Table)view.getElement("objects");
         
         insertItem(objects);
     }
     
-    public final void download(ViewData view) throws Exception {
-        String filename = (String)view.getParameter("instructionname");
-        String[] lines = (String[])view.getParameter("list");
-        
-        for (String line : lines)
-            view.print(line+"\n");
-        
-        view.setContentType("text/plain");
-        view.setHeader("Content-Disposition", "attachment; filename=\"" +
-                filename + "\"");
-    }
-    
-    public final void generate(ViewData view) throws Exception {
-        String name;
-        Table objects = (Table)view.getElement("objects");
-        List<String> lines, instructions = new ArrayList<String>();
-        Documents documents = new Documents(this);
-        
-        instructions.add(IDTAG);
-        
-        for (TableItem item : objects.getItens()) {
-            name = ((InputComponent)item.get("object")).getValue();
-            
-            if (name.equals(""))
-                continue;
-            
-            lines = getInstructions(name, documents);
-            
-            if (lines == null) {
-                view.message(Const.ERROR, "model.not.found");
-                return;
-            }
-            
-            instructions.addAll(lines);
-        }
-        
-        if (instructions.size() == 1)
-            return;
-        
-        name = new StringBuilder("IOCASTE_BUILD_INSTRUCTIONS_").
-                append(Calendar.getInstance().getTime().getTime()).
-                append(".txt").toString();
-        
-        view.setReloadableView(true);
-        view.export("list", instructions.toArray(new String[0]));
-        view.export("instructionname", name);
-        view.redirect(null, "download");
-    }
-    
-    public final List<String> getInstructions(String name, Documents documents) 
+    /**
+     * 
+     * @param filename
+     * @param function
+     * @return
+     * @throws Exception
+     */
+    private final int build(String filename, Function function)
             throws Exception {
-        DataElement dataelement;
-        StringBuilder sb;
-        List<String> lines;
-        DocumentModel model;
-        Set<DocumentModelItem> itens;
-        
-        if (!documents.hasModel(name))
-            return null;
-        
-        lines = new ArrayList<String>();  
-
-        model = documents.getModel(name);
-        itens = model.getItens();
-        
-        sb = new StringBuilder(model.getName()).append(";").
-                append(model.getTableName()).append(";").
-                append(model.getClassName()).append(";").
-                append(itens.size());
-        
-        lines.add(sb.toString());
-        for (DocumentModelItem item : itens) {
-            dataelement = item.getDataElement();
-            
-            sb = new StringBuilder(item.getName()).append(";").
-                    append(item.getTableFieldName()).append(";").
-                    append(item.getAttributeName()).append(";").
-                    append(dataelement.getName()).append(";").
-                    append(dataelement.getType()).append(";").
-                    append(dataelement.getLength()).append(";").
-                    append(dataelement.getDecimals()).append(";").
-                    append(dataelement.isUpcase()).append(";").
-                    append(model.isKey(item));
-            
-            lines.add(sb.toString());
-        }
-        
-        return lines;
-    }
-
-    private final void insertItem(Table table) throws Exception {
-        Documents documents = new Documents(this);
-        TableItem item = new TableItem(table);
-        TextField tfield = new TextField(table, "name");
-        SearchHelp sh = new SearchHelp(table, "shname");
-        
-        sh.setModelName("MODEL");
-        sh.addModelItemName("NAME");
-        sh.setExport("NAME");
-        
-        tfield.setDataElement(documents.getDataElement("MODEL.NAME"));
-        tfield.setSearchHelp(sh);
-        
-        item.add(tfield);
-    }
-    
-    public void main(ViewData view) throws Exception {
-        Container container = new Form(null, "main");
-        Form uploadcontainer = new Form(null, "upldcntr");
-        Frame exportframe = new Frame(container, "export");
-        Frame importframe = new Frame(uploadcontainer, "import");
-        Table objects = new Table(exportframe, "objects");
-        
-        new TableColumn(objects, "object");
-        
-        insertItem(objects);
-        
-        new Button(exportframe, "add");
-        new Button(exportframe, "generate");
-        
-        uploadcontainer.setEnctype("multipart/form-data");
-        
-        new FileEntry(importframe, "buildfile");
-        new Button(importframe, "upload");
-        
-        view.addContainer(container);
-        view.addContainer(uploadcontainer);
-        view.setNavbarActionEnabled("back", true);
-        view.setTitle("transport-utility");
-    }
-    
-    public void upload(ViewData view) throws Exception {
         int currentline, nitens, pass;
-        File file;
-        FileReader freader;
-        BufferedReader breader;
-        List<String> list;
         String[] parsed;
         DocumentModel model;
         DocumentModelItem modelitem;
         DocumentModelKey key;
         DataElement dataelement;
         Documents documents;
-        FileEntry fileentry = (FileEntry)view.getElement("buildfile");
-        String filename = fileentry.getValue();
-        
-        switch (fileentry.getError()) {
-        case FileEntry.FILE_NOT_FOUND:
-            view.message(Const.ERROR, "file.not.found");
-            return;
-        case FileEntry.EMPTY_FILE_NAME:
-            view.message(Const.ERROR, "filename.is.obligatory");
-            return;
-        }
-        
-        file = new File(filename);
-        freader = new FileReader(file);
-        breader = new BufferedReader(freader);
-        list = new ArrayList<String>();
+        File file = new File(filename);
+        FileReader freader = new FileReader(file);
+        BufferedReader breader = new BufferedReader(freader);
+        List<String> list = new ArrayList<String>();
         
         while (breader.ready())
             list.add(breader.readLine());
         
         breader.close();
         
-        if (list.size() == 0) {
-            view.message(Const.ERROR, "file.is.empty");
-            return;
-        }
+        if (list.size() == 0)
+            return FILE_IS_EMPTY;
         
         pass = 0;
         currentline = 0;
@@ -216,20 +86,16 @@ public class Main extends AbstractPage {
         for (String line : list) {
             switch (pass) {
             case 0:
-                if (!line.equals(IDTAG)) {
-                    view.message(Const.ERROR, "invalid.file");
-                    return;
-                }
+                if (!line.equals(IDTAG))
+                    return INVALID_FILE;
                 
                 pass = 1;
                 continue;
             case 1:
                 parsed = line.split(";");
                 
-                if (parsed.length != 4) {
-                    view.message(Const.ERROR, "invalid.header");
-                    return;
-                }
+                if (parsed.length != 4)
+                    return INVALID_HEADER;
                 
                 model = new DocumentModel();
                 model.setName(parsed[0]);
@@ -286,6 +152,283 @@ public class Main extends AbstractPage {
                 
                 continue;
             }
+        }
+        
+        return 0;
+    }
+    
+    /**
+     * 
+     * @param view
+     * @throws Exception
+     */
+    public final void download(ViewData view) throws Exception {
+        String filename = (String)view.getParameter("instructionname");
+        String[] lines = (String[])view.getParameter("list");
+        
+        for (String line : lines)
+            view.print(line+"\n");
+        
+        view.setContentType("text/plain");
+        view.setHeader("Content-Disposition", "attachment; filename=\"" +
+                filename + "\"");
+    }
+    
+    /**
+     * 
+     * @param view
+     * @throws Exception
+     */
+    public final void generate(ViewData view) throws Exception {
+        String name;
+        Table objects = (Table)view.getElement("objects");
+        List<String> lines, instructions = new ArrayList<String>();
+        Documents documents = new Documents(this);
+        
+        instructions.add(IDTAG);
+        
+        for (TableItem item : objects.getItens()) {
+            name = ((InputComponent)item.get("object")).getValue();
+            
+            if (name.equals(""))
+                continue;
+            
+            lines = getInstructions(name, documents);
+            
+            if (lines == null) {
+                view.message(Const.ERROR, "model.not.found");
+                return;
+            }
+            
+            instructions.addAll(lines);
+        }
+        
+        if (instructions.size() == 1)
+            return;
+        
+        name = new StringBuilder("IOCASTE_BUILD_INSTRUCTIONS_").
+                append(Calendar.getInstance().getTime().getTime()).
+                append(".txt").toString();
+        
+        view.setReloadableView(true);
+        view.export("list", instructions.toArray(new String[0]));
+        view.export("instructionname", name);
+        view.redirect(null, "download");
+    }
+    
+    /**
+     * 
+     * @param name
+     * @param documents
+     * @return
+     * @throws Exception
+     */
+    public final List<String> getInstructions(String name, Documents documents) 
+            throws Exception {
+        DataElement dataelement;
+        StringBuilder sb;
+        List<String> lines;
+        DocumentModel model;
+        Set<DocumentModelItem> itens;
+        
+        if (!documents.hasModel(name))
+            return null;
+        
+        lines = new ArrayList<String>();  
+
+        model = documents.getModel(name);
+        itens = model.getItens();
+        
+        sb = new StringBuilder(model.getName()).append(";").
+                append(model.getTableName()).append(";").
+                append(model.getClassName()).append(";").
+                append(itens.size());
+        
+        lines.add(sb.toString());
+        for (DocumentModelItem item : itens) {
+            dataelement = item.getDataElement();
+            
+            sb = new StringBuilder(item.getName()).append(";").
+                    append(item.getTableFieldName()).append(";").
+                    append(item.getAttributeName()).append(";").
+                    append(dataelement.getName()).append(";").
+                    append(dataelement.getType()).append(";").
+                    append(dataelement.getLength()).append(";").
+                    append(dataelement.getDecimals()).append(";").
+                    append(dataelement.isUpcase()).append(";").
+                    append(model.isKey(item));
+            
+            lines.add(sb.toString());
+        }
+        
+        return lines;
+    }
+
+    /**
+     * 
+     * @param view
+     * @throws Exception
+     */
+    public final void importitens(ViewData view) throws Exception {
+        String filename = null;
+        Table table = (Table)view.getElement("pool");
+        
+        for (TableItem item : table.getItens()) {
+            if (!item.isSelected())
+                continue;
+            
+            filename = getRealPath("WEB-INF", "pool",
+                    ((Text)item.get("filename")).getName());
+            
+            switch (build(filename, this)) {
+            case FILE_IS_EMPTY:
+                view.message(Const.ERROR, "file.is.empty");
+                return;
+                
+            case INVALID_FILE:
+                view.message(Const.ERROR, "invalid.file");
+                return;
+                
+            case INVALID_HEADER:
+                view.message(Const.ERROR, "invalid.header");
+                return;
+            }
+        }
+        
+        if (filename == null)
+            return;
+        
+        view.message(Const.STATUS, "importing-successful");
+    }
+    
+    /**
+     * 
+     * @param table
+     * @throws Exception
+     */
+    private final void insertItem(Table table) throws Exception {
+        Documents documents = new Documents(this);
+        TableItem item = new TableItem(table);
+        TextField tfield = new TextField(table, "name");
+        SearchHelp sh = new SearchHelp(table, "shname");
+        
+        sh.setModelName("MODEL");
+        sh.addModelItemName("NAME");
+        sh.setExport("NAME");
+        
+        tfield.setDataElement(documents.getDataElement("MODEL.NAME"));
+        tfield.setSearchHelp(sh);
+        
+        item.add(tfield);
+    }
+    
+    /**
+     * 
+     * @param view
+     * @throws Exception
+     */
+    public void list(ViewData view) throws Exception {
+        String[] files = (String[])view.getParameter("files");
+        Container container = new Form(null, "list");
+        Table table = new Table(container, "pool");
+        
+        new TableColumn(table, "filename");
+        table.setHeader(false);
+        table.setMark(true);
+        
+        for (String file : files)
+            new TableItem(table).add(new Text(table, file));
+        
+        new Button(container, "importitens");
+        
+        view.setNavbarActionEnabled("back", true);
+        view.setTitle("importable-build-instructions");
+        view.addContainer(container);
+    }
+    
+    /**
+     * 
+     * @param view
+     * @throws Exception
+     */
+    public void main(ViewData view) throws Exception {
+        TableItem importtbitem;
+        Container container = new Form(null, "main");
+        Form uploadcontainer = new Form(null, "upldcntr");
+        Frame exportframe = new Frame(container, "export");
+        Frame importframe = new Frame(uploadcontainer, "import");
+        Table importtable, objects = new Table(exportframe, "objects");
+        
+        new TableColumn(objects, "object");
+        
+        insertItem(objects);
+        
+        new Button(exportframe, "add");
+        new Button(exportframe, "generate");
+        
+        uploadcontainer.setEnctype("multipart/form-data");
+        
+        importtable = new Table(importframe, "importtable");
+        importtable.setHeader(false);
+        
+        new TableColumn(importtable, "a");
+        
+        importtbitem = new TableItem(importtable);
+        importtbitem.add(new FileEntry(importtable, "buildfile"));
+        importtbitem = new TableItem(importtable);
+        importtbitem.add(new Button(importtable, "upload"));
+        importtbitem = new TableItem(importtable);
+        importtbitem.add(new Link(importtable, "frompool", "pool"));
+        
+        view.addContainer(container);
+        view.addContainer(uploadcontainer);
+        view.setNavbarActionEnabled("back", true);
+        view.setTitle("transport-utility");
+    }
+    
+    /**
+     * 
+     * @param view
+     * @throws Exception
+     */
+    public void pool(ViewData view) throws Exception {
+        File file = new File(getRealPath("WEB-INF", "pool"));
+        
+        view.setReloadableView(true);
+        view.export("files", file.list());
+        view.redirect(null, "list");
+    }
+    
+    /**
+     * 
+     * @param view
+     * @throws Exception
+     */
+    public void upload(ViewData view) throws Exception {
+        FileEntry fileentry = (FileEntry)view.getElement("buildfile");
+        String filename = fileentry.getValue();
+        
+        switch (fileentry.getError()) {
+        case FileEntry.FILE_NOT_FOUND:
+            view.message(Const.ERROR, "file.not.found");
+            return;
+        case FileEntry.EMPTY_FILE_NAME:
+            view.message(Const.ERROR, "filename.is.obligatory");
+            return;
+        }
+        
+        switch (build(filename, this)) {
+        case FILE_IS_EMPTY:
+            view.message(Const.ERROR, "file.is.empty");
+            return;
+            
+        case INVALID_FILE:
+            view.message(Const.ERROR, "invalid.file");
+            return;
+            
+        case INVALID_HEADER:
+            view.message(Const.ERROR, "invalid.header");
+            return;
         }
         
         view.message(Const.STATUS, "model.imported.successfully");
