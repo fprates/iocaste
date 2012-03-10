36b43bc9b37e68ab46bf21cdaaf0d7f49fe3c9b9
Francisco Prates <mad.kanie@gmail.com>
2012-03-10 02:27:22 -0300

    scripts: insere um pouco mais de informação para modelo login.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- scripts/db-install-25-login.sql
+++ scripts/db-install-25-login.sql
@@ -1,8 +1,9 @@
 \p running login script.
-insert into docs001 (docid) values ('LOGIN');
+insert into docs001(docid, tname) values ('LOGIN', 'USERS001');
+insert into docs002(iname, docid, index, fname) values ('LOGIN.USERNAME', 'LOGIN', 0, 'UNAME');
+insert into docs002(iname, docid, index, fname) values ('LOGIN.SECRET', 'LOGIN', 1, 'SECRT');
+insert into docs004(iname, docid) values('LOGIN.USERNAME', 'LOGIN');
 
-insert into docs002 (iname, docid, index) values ('LOGIN.USERNAME', 'LOGIN', 0);
-insert into docs002 (iname, docid, index) values ('LOGIN.SECRET', 'LOGIN', 1);
 \p initial configuration saved.
 
 commit work;
