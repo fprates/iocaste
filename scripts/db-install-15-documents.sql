f9dc5921b217b33b5ff946ffd06437c5222b29a4
Francisco Prates <mad.kanie@gmail.com>
2012-03-11 13:35:29 -0300

    scripts: atualização para referência tabela-modelo.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- scripts/db-install-15-documents.sql
+++ scripts/db-install-15-documents.sql
@@ -47,6 +47,12 @@ create table docs004 (
    docid varchar(24) foreign key references docs001(docid)
 );
 
+/* ligação tabela-modelo */
+create table docs005 (
+    tname varchar(12) primary key,
+    docid varchar(24) foreign key references docs001(docid)
+);
+
 \p document tables has been generated
 
 grant select, insert, update, delete on range001 to iocastedb;
@@ -54,6 +60,7 @@ grant select, insert, update, delete on docs001 to iocastedb;
 grant select, insert, update, delete on docs002 to iocastedb;
 grant select, insert, update, delete on docs003 to iocastedb;
 grant select, insert, update, delete on docs004 to iocastedb;
+grant select, insert, update, delete on docs005 to iocastedb;
 \p permissions granted.
 
 insert into docs001(docid, tname, class) values('MODEL', 'DOCS001', 'org.iocaste.documents.common.DocumentModel');
@@ -64,6 +71,7 @@ insert into docs003(ename, decim, lngth, etype, upcas) values('MODEL.NAME', 0, 2
 insert into docs003(ename, decim, lngth, etype, upcas) values('MODEL.TABLE', 0, 12, 0, 1);
 insert into docs003(ename, decim, lngth, etype, upcas) values('MODEL.CLASS', 0, 255, 0, 0);
 insert into docs004(iname, docid) values('MODEL.NAME', 'MODEL');
+insert into docs005(tname, docid) values('DOCS001', 'MODEL');
 
 insert into docs001(docid, tname, class) values('MODELITEM', 'DOCS002', 'org.iocaste.documents.common.DocumentModelItem');
 insert into docs002(iname, docid, index, fname, ename, attrb) values('MODELITEM.NAME', 'MODELITEM', 0, 'INAME', 'MODELITEM.NAME', 'name');
@@ -79,6 +87,7 @@ insert into docs003(ename, decim, lngth, etype, upcas) values('MODELITEM.INDEX',
 insert into docs003(ename, decim, lngth, etype, upcas) values('MODELITEM.FIELDNAME', 0, 12, 0, 1);
 insert into docs003(ename, decim, lngth, etype, upcas) values('MODELITEM.ATTRIB', 0, 64, 0, 0);
 insert into docs004(iname, docid) values('MODELITEM.NAME', 'MODELITEM');
+insert into docs005(tname, docid) values('DOCS002', 'MODELITEM');
 
 insert into docs001(docid, tname, class) values('DATAELEMENT', 'DOCS003', 'org.iocaste.documents.common.DataElement');
 insert into docs002(iname, docid, index, fname, ename, attrb) values('DATAELEMENT.NAME', 'DATAELEMENT', 0, 'ENAME', 'DATAELEMENT.NAME', 'name');
@@ -92,6 +101,13 @@ insert into docs003(ename, decim, lngth, etype, upcas) values('DATAELEMENT.LENGT
 insert into docs003(ename, decim, lngth, etype, upcas) values('DATAELEMENT.TYPE', 0, 1, 3, 0);
 insert into docs003(ename, decim, lngth, etype, upcas) values('DATAELEMENT.UPCASE', 0, 1, 1, 0);
 insert into docs004(iname, docid) values('DATAELEMENT.NAME', 'DATAELEMENT');
+insert into docs005(tname, docid) values('DOCS003', 'DATAELEMENT');
+
+insert into docs001(docid, tname, class) values('TABLE_MODEL', 'DOCS005', '');
+insert into docs002(iname, docid, index, fname, ename, attrb) values('TABLE_MODEL.TABLE', 'TABLE_MODEL', 0, 'TNAME', 'MODEL.TABLE', 'tableName');
+insert into docs002(iname, docid, index, fname, ename, attrb, mdref, itref) values('TABLE_MODEL.MODEL', 'TABLE_MODEL', 1, 'MODEL', 'MODEL.NAME', 'model', 'MODEL', 'NAME');
+insert into docs004(iname, docid) values('TABLE_MODEL.TABLE', 'TABLE_MODEL');
+insert into docs005(tname, docid) values('DOCS005', 'TABLE_MODEL');
 
 insert into docs001(docid, tname, class) values('NUMBER_RANGE', 'RANGE001', '');
 insert into docs002(iname, docid, index, fname, ename, attrb) values('NUMBER_RANGE.IDENT', 'NUMBER_RANGE', 0, 'IDENT', 'NUMBER_RANGE.IDENT', '');
@@ -99,6 +115,7 @@ insert into docs002(iname, docid, index, fname, ename, attrb) values('NUMBER_RAN
 insert into docs003(ename, decim, lngth, etype, upcas) values('NUMBER_RANGE.IDENT', 0, 12, 0, 1);
 insert into docs003(ename, decim, lngth, etype, upcas) values('NUMBER_RANGE.CURRENT', 0, 12, 3, 0);
 insert into docs004(iname, docid) values('NUMBER_RANGE.IDENT', 'NUMBER_RANGE');
+insert into docs005(tname, docid) values('RANGE001', 'NUMBER_RANGE');
 
 commit work;
 
--- scripts/db-install-25-login.sql
+++ scripts/db-install-25-login.sql
@@ -3,6 +3,7 @@ insert into docs001(docid, tname) values ('LOGIN', 'USERS001');
 insert into docs002(iname, docid, index, fname) values ('LOGIN.USERNAME', 'LOGIN', 0, 'UNAME');
 insert into docs002(iname, docid, index, fname) values ('LOGIN.SECRET', 'LOGIN', 1, 'SECRT');
 insert into docs004(iname, docid) values('LOGIN.USERNAME', 'LOGIN');
+insert into docs005(tname, docid) values('USERS001', 'LOGIN');
 
 \p initial configuration saved.
 
