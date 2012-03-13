9cc5a27b0ec8d38c52f3d1e4d6608eb7e7fa3217
Francisco Prates <mad.kanie@gmail.com>
2012-03-13 01:40:51 -0300

    shell: Button passa a utilizar htmlname.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- shell/classes/org/iocaste/shell/renderer/ButtonRenderer.java
+++ shell/classes/org/iocaste/shell/renderer/ButtonRenderer.java
@@ -13,18 +13,20 @@ public class ButtonRenderer extends Renderer {
     public static final XMLElement render(Button button, Config config) {
         String text_ = button.getText();
         String name = button.getName();
+        String htmlname = button.getHtmlName();
         XMLElement buttontag= new XMLElement("input");
         
         if (text_ == null)
             text_ = name;
         
         buttontag.add("type", (!button.isSubmit())? "button" : "submit");
-        buttontag.add("name", name);
-        buttontag.add("id", name);
+        buttontag.add("name", htmlname);
+        buttontag.add("id", htmlname);
         buttontag.add("class", button.getStyleClass());
         buttontag.add("value", config.getText(text_, name));
-        buttontag.add("onClick", "defineAction('" +
-        		config.getCurrentAction() + "', '" + name + "')");
+        buttontag.add("onClick", new StringBuilder("defineAction('").
+                append(config.getCurrentAction()).append("', '").
+                append(htmlname).append("')").toString());
         
         addAttributes(buttontag, button);
         
