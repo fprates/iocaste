ccb962b5acf8d6a1a76a6d57bdfe939943adde81
Francisco Prates <mad.kanie@gmail.com>
2012-03-16 21:21:38 -0300

    packagetool: implementa geração de objetos de numeração e inserção de registros.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- packagetool/classes/org/iocaste/packagetool/Services.java
+++ packagetool/classes/org/iocaste/packagetool/Services.java
@@ -1,11 +1,13 @@
 package org.iocaste.packagetool;
 
+import java.util.Map;
+
 import org.iocaste.documents.common.DocumentModel;
+import org.iocaste.documents.common.DocumentModelItem;
 import org.iocaste.documents.common.Documents;
-//import org.iocaste.documents.common.ExtendedObject;
+import org.iocaste.documents.common.ExtendedObject;
 import org.iocaste.packagetool.common.InstallData;
 import org.iocaste.protocol.AbstractFunction;
-//import org.iocaste.protocol.Iocaste;
 import org.iocaste.protocol.Message;
 
 public class Services extends AbstractFunction {
@@ -15,7 +17,11 @@ public class Services extends AbstractFunction {
     }
     
     public final byte install(Message message) throws Exception {
-//        ExtendedObject object;
+        ExtendedObject object;
+        DocumentModel tasks;
+        Object[] values;
+        int i;
+        Map<String, String> links;
         InstallData data = (InstallData)message.get("data");
         Documents documents = new Documents(this);
         
@@ -24,12 +30,31 @@ public class Services extends AbstractFunction {
                 documents.updateModel(model);
             else
                 documents.createModel(model);
-//            
-//            object = new ExtendedObject(model);
-//            
-//            for (String key : data.get)
-//            documents.save(object);
+            
+            values = data.getValues(model);
+            if (values == null)
+                continue;
+            
+            i = 0;
+            object = new ExtendedObject(model);
+            for (DocumentModelItem modelitem : model.getItens())
+                object.setValue(modelitem, values[i++]);
+            
+            documents.save(object);
         }
+        
+        tasks = documents.getModel("TASKS");
+        links = data.getLinks();
+        for (String link : links.keySet()) {
+            object = new ExtendedObject(tasks);
+            object.setValue("NAME", link);
+            object.setValue("COMMAND", links.get(link));
+            
+            documents.save(object);
+        }
+        
+        for (String factory : data.getNumberFactories())
+            documents.createNumberFactory(factory);
         
         return 0;
     }
--- packagetool/lib/org/iocaste/packagetool/common/InstallData.java
+++ packagetool/lib/org/iocaste/packagetool/common/InstallData.java
@@ -3,8 +3,10 @@ package org.iocaste.packagetool.common;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.iocaste.documents.common.DataElement;
 import org.iocaste.documents.common.DocumentModel;
@@ -15,10 +17,14 @@ public class InstallData implements Serializable {
     private static final long serialVersionUID = -4509980464670421174L;
     private List<DocumentModel> models;
     private Map<DocumentModel, Object[]> values;
+    private Map<String, String> links;
+    private Set<String> numbers;
     
     public InstallData() {
         models = new ArrayList<DocumentModel>();
         values = new HashMap<DocumentModel, Object[]>();
+        links = new HashMap<String, String>();
+        numbers = new HashSet<String>();
     }
     
     /**
@@ -37,10 +43,30 @@ public class InstallData implements Serializable {
      * @param element
      * @param attribute
      * @param iskey
+     * @return
      */
-    public final void addModelItem(DocumentModel model, String name,
+    public final DocumentModelItem addModelItem(DocumentModel model,
+            String name, String fieldname, DataElement element,
+            String attribute, boolean iskey) {
+        
+        return addModelItem(model, name, fieldname, element, attribute, iskey,
+                null);
+    }
+    
+    /**
+     * 
+     * @param model
+     * @param name
+     * @param fieldname
+     * @param element
+     * @param attribute
+     * @param iskey
+     * @param reference
+     * @return
+     */
+    public final DocumentModelItem addModelItem(DocumentModel model, String name,
             String fieldname, DataElement element, String attribute,
-            boolean iskey) {
+            boolean iskey, DocumentModelItem reference) {
         DocumentModelItem item;
         DocumentModelKey key;
         
@@ -51,18 +77,27 @@ public class InstallData implements Serializable {
         item.setAttributeName(attribute);
         item.setIndex(model.getItens().length);
         item.setDocumentModel(model);
+        item.setReference(reference);
         
         model.add(item);
         
         if (!iskey)
-            return;
+            return item;
         
         key = new DocumentModelKey();
         key.setModel(model);
         key.setModelItem(name);
         model.addKey(key);
         
-        return;
+        return item;
+    }
+    
+    /**
+     * 
+     * @param name
+     */
+    public final void addNumberFactory(String name) {
+        numbers.add(name);
     }
     
     /**
@@ -70,7 +105,7 @@ public class InstallData implements Serializable {
      * @param model
      * @param values
      */
-    public final void addValues(DocumentModel model, Object... values) {        
+    public final void addValues(DocumentModel model, Object... values) {
         this.values.put(model, values);
     }
     
@@ -96,6 +131,21 @@ public class InstallData implements Serializable {
         return element;
     }
     
+    /**
+     * 
+     * @return
+     */
+    public final Map<String, String> getLinks() {
+        return links;
+    }
+    
+    /**
+     * 
+     * @param name
+     * @param tablename
+     * @param classname
+     * @return
+     */
     public final DocumentModel getModel(String name, String tablename,
             String classname) {
         DocumentModel model = new DocumentModel();
@@ -108,6 +158,7 @@ public class InstallData implements Serializable {
         
         return model;
     }
+    
     /**
      * 
      * @return
@@ -118,10 +169,27 @@ public class InstallData implements Serializable {
     
     /**
      * 
+     * @return
+     */
+    public final String[] getNumberFactories() {
+        return numbers.toArray(new String[0]);
+    }
+    
+    /**
+     * 
      * @param model
      * @return
      */
     public final Object[] getValues(DocumentModel model) {
         return values.get(model);
+    }
+    
+    /**
+     * 
+     * @param link
+     * @param command
+     */
+    public final void link(String link, String command) {
+        links.put(link, command);
     }
 }
