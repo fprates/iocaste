59c2ebc0229af164bfdf9e85bd7a6360e347b819
Francisco Prates <mad.kanie@gmail.com>
2012-03-21 01:25:35 -0300

    style-editor: implementação inicial de editor de folhas de estilo.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

---
+++ styleeditor/classes/org/iocaste/styleeditor/Main.java
@@ -0,0 +1,45 @@
+package org.iocaste.styleeditor;
+
+import org.iocaste.documents.common.Documents;
+import org.iocaste.packagetool.common.InstallData;
+import org.iocaste.protocol.Message;
+import org.iocaste.shell.common.AbstractPage;
+import org.iocaste.shell.common.Button;
+import org.iocaste.shell.common.Const;
+import org.iocaste.shell.common.Container;
+import org.iocaste.shell.common.DataForm;
+import org.iocaste.shell.common.DataItem;
+import org.iocaste.shell.common.Form;
+import org.iocaste.shell.common.ViewData;
+
+public class Main extends AbstractPage {
+
+    public Main() {
+        export("install", "install");
+    }
+    
+    public final InstallData install(Message message) {
+        InstallData data = new InstallData();
+        
+        data.link("STYLE", "iocaste-styleeditor");
+        
+        return data;
+    }
+    
+    public final void main(ViewData view) throws Exception {
+        Container container = new Form(null, "main");
+        DataForm form = new DataForm(container, "selection");
+        DataItem item = new DataItem(form, Const.TEXT_FIELD, "estilo");
+        
+        item.setModelItem(new Documents(this).getModel("STYLE").
+                getModelItem("NAME"));
+        
+        new Button(container, "create");
+        new Button(container, "show");
+        new Button(container, "update");
+        
+        view.setNavbarActionEnabled("back", true);
+        view.addContainer(container);
+        view.setTitle("style-editor");
+    }
+}
