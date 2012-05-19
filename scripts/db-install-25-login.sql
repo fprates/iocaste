c5822c2e17c25afcb1ca7d12a7a5b3262a1d0c3e
Francisco Prates <mad.kanie@gmail.com>
2012-05-19 20:25:28 -0300

    login: completa modelo de login.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- scripts/db-install-25-login.sql
+++ scripts/db-install-25-login.sql
@@ -1,7 +1,9 @@
 \p running login script.
 insert into docs001(docid, tname) values ('LOGIN', 'USERS001');
-insert into docs002(iname, docid, index, fname) values ('LOGIN.USERNAME', 'LOGIN', 0, 'UNAME');
-insert into docs002(iname, docid, index, fname) values ('LOGIN.SECRET', 'LOGIN', 1, 'SECRT');
+insert into docs003(ename, decim, lngth, etype, upcas) values('LOGIN.USERNAME', 0, 12, 0, 1);
+insert into docs003(ename, decim, lngth, etype, upcas) values('LOGIN.SECRET', 0, 12, 0, 0);
+insert into docs002(iname, docid, index, fname, ename) values ('LOGIN.USERNAME', 'LOGIN', 0, 'UNAME', 'LOGIN.USERNAME');
+insert into docs002(iname, docid, index, fname, ename) values ('LOGIN.SECRET', 'LOGIN', 1, 'SECRT', 'LOGIN.SECRET');
 insert into docs004(iname, docid) values('LOGIN.USERNAME', 'LOGIN');
 insert into docs005(tname, docid) values('USERS001', 'LOGIN');
 
