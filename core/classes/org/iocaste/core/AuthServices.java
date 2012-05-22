3ba34fe90aaaba606b407c5d9939303914dda4f6
Francisco Prates <mad.kanie@gmail.com>
2012-05-21 21:42:46 -0300

    core: implementaçao de perfil do usuario.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- core/classes/org/iocaste/core/AuthServices.java
+++ core/classes/org/iocaste/core/AuthServices.java
@@ -1,5 +1,6 @@
 package org.iocaste.core;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +10,10 @@ import org.iocaste.protocol.user.Authorization;
 
 public class AuthServices {
 
+    private static final int bdToInt(Object object) {
+        return ((BigDecimal)object).intValue();
+    }
+    
     /**
      * 
      * @param connection
@@ -21,36 +26,50 @@ public class AuthServices {
     @SuppressWarnings("unchecked")
     public static final Authorization[] getAuthorization(Connection connection,
             DBServices db, String username, String authname) throws Exception {
-        Authorization autobject;
-        List<Authorization> autobjects;
+        int ident;
+        Authorization authorization;
         Map<String, Object> resultmap;
-        String name, value, query =
-                "select * from USERS002 where UNAME = ? and AUTNM = ?";
-        Object[] result_, result =
-                db.select(connection, query, 0, username, authname);
+        List<Authorization> authlist;
+        String name, value, query = "select * from USERS004 where UNAME = ?";
+        Object[] prmresult, authresult, prfresult =
+                db.select(connection, query, 0, username);
         
-        if (result == null)
+        if (prfresult == null)
             return null;
         
-        autobjects = new ArrayList<Authorization>();
-        for (Object object : result) {
-            resultmap = (Map<String, Object>)object;
+        authlist = new ArrayList<Authorization>();
+        
+        for (Object prfobject : prfresult) {
+            resultmap = (Map<String, Object>)prfobject;
+            ident = bdToInt(resultmap.get("IDENT"));
+            query = "select * from USERS002 where PRFID = ? and AUTNM = ?";
+            authresult = db.select(connection, query, 0, ident, authname);
+            
+            if (authresult == null)
+                continue;
             
             query = "select * from USERS003 where AUTID = ?";
-            result_ = db.select(connection, query, 0, resultmap.get("IDENT"));
-            autobject = new Authorization();
-            autobject.setName(authname);
-            autobjects.add(autobject);
             
-            for (Object object_ : result_) {
-                resultmap = (Map<String, Object>)object_;
-                name = (String)resultmap.get("PARAM");
-                value = (String)resultmap.get("VALUE");
-                autobject.add(name, value);
+            for (Object authobject : authresult) {
+                resultmap = (Map<String, Object>)authobject;
+                
+                ident = bdToInt(resultmap.get("IDENT"));
+                prmresult = db.select(connection, query, 0, ident);
+                
+                authorization = new Authorization();
+                authorization.setName(authname);
+                authlist.add(authorization);
+                
+                for (Object prmobject : prmresult) {
+                    resultmap = (Map<String, Object>)prmobject;
+                    name = (String)resultmap.get("PARAM");
+                    value = (String)resultmap.get("VALUE");
+                    authorization.add(name, value);
+                }
             }
         }
         
-        return (autobjects.size() == 0)? null :
-            autobjects.toArray(new Authorization[0]);
+        return (authlist.size() == 0)? null :
+            authlist.toArray(new Authorization[0]);
     }
 }
--- scripts/db-install-10-core.sql
+++ scripts/db-install-10-core.sql
@@ -4,6 +4,7 @@ create user iocastedb password initial;
 
 drop table users003 if exists;
 drop table users002 if exists;
+drop table users004 if exists;
 drop table users001 if exists;
 \p core tables dropped.
 
@@ -16,15 +17,21 @@ create table users001 (
    usrid numeric(6)
 );
 
-create table users002 (
+create table users004 (
    ident numeric(9) primary key,
    uname varchar(12) foreign key references users001(uname),
+   prfnm varchar(12)
+);
+   
+create table users002 (
+   ident numeric(12) primary key,
+   prfid numeric(9) foreign key references users004(ident),
    autnm varchar(24)
 );
 
 create table users003 (
-   ident numeric(12) primary key,
-   autid numeric(9) foreign key references users002(ident),
+   ident numeric(15) primary key,
+   autid numeric(12) foreign key references users002(ident),
    param varchar(12),
    value varchar(64)
 );
@@ -34,13 +41,15 @@ create table users003 (
 grant select, insert, update, delete on users001 to iocastedb;
 grant select, insert, update, delete on users002 to iocastedb;
 grant select, insert, update, delete on users003 to iocastedb;
+grant select, insert, update, delete on users004 to iocastedb;
 \p permissions granted.
 
 insert into users001(uname, secrt, fname, sname, usrid) values('ADMIN', 'iocaste', 'Administrator', '', 1);
-insert into users002(ident, uname, autnm) values(1001, 'ADMIN', 'APPLICATION.EXECUTE');
-insert into users003(ident, autid, param, value) values(1001001, 1001, 'APPNAME', 'iocaste-tasksel');
-insert into users002(ident, uname, autnm) values(1002, 'ADMIN', 'APPLICATION.EXECUTE');
-insert into users003(ident, autid, param, value) values(1002001, 1002, 'APPNAME', 'iocaste-packagetool');
+insert into users004(ident, uname, prfnm) values(1001, 'ADMIN', 'ALL');
+insert into users002(ident, prfid, autnm) values(1001001, 1001, 'APPLICATION.EXECUTE');
+insert into users003(ident, autid, param, value) values(1001001001, 1001001, 'APPNAME', 'iocaste-tasksel');
+insert into users002(ident, prfid, autnm) values(1002001, 1001, 'APPLICATION.EXECUTE');
+insert into users003(ident, autid, param, value) values(1001002001, 1002001, 'APPNAME', 'iocaste-packagetool');
 \p initial configuration saved.
 
 commit work;
