5dcc4915d270cbb7b83d8f000be8000002baea73
Francisco Prates <mad.kanie@gmail.com>
2012-05-22 23:23:46 -0300

    dataview: inclui autorizaçao de execuçao.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- dataview/classes/org/iocaste/dataview/Install.java
+++ dataview/classes/org/iocaste/dataview/Install.java
@@ -1,12 +1,19 @@
 package org.iocaste.dataview;
 
 import org.iocaste.packagetool.common.InstallData;
+import org.iocaste.protocol.user.Authorization;
 
 public class Install {
 
     public static final InstallData self() {
         InstallData data = new InstallData();
-
+        Authorization authorization = new Authorization("APPLICATION.EXECUTE");
+        
+        authorization.setObject("APPLICATION");
+        authorization.setAction("EXECUTE");
+        authorization.add("APPNAME", "iocaste-dataview");
+        data.add(authorization);
+        
         data.link("DATAVIEW", "iocaste-dataview");
         data.link("SE16", "iocaste-dataview");
         
