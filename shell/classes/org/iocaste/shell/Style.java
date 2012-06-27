d5310a8ac4c523cc72ce9c89fbb48db2204c4fe4
Francisco Prates <mad.kanie@gmail.com>
2012-06-27 20:42:04 -0300

    shell: melhora performance para recuperar estilo da pagina.
    seleciona estilo da pagina atraves de join.
    
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- shell/classes/org/iocaste/shell/Style.java
+++ shell/classes/org/iocaste/shell/Style.java
@@ -1,6 +1,5 @@
 package org.iocaste.shell;
 
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -11,13 +10,14 @@ import org.iocaste.protocol.Iocaste;
 public class Style {
 
     private static final Object[] checkedSelect(Iocaste iocaste, String from,
-            String where, Object... criteria) throws Exception {
+            Map<String, String[]> ijoin, String where, Object... criteria)
+                    throws Exception {
         Map<String, Object> parameters = new HashMap<String, Object>();
         
         parameters.put("from", from);
         parameters.put("where", where);
         parameters.put("criteria", criteria);
-        
+        parameters.put("inner_join", ijoin);
         return (Object[])iocaste.callAuthorized("checked_select",
                 parameters);
     }
@@ -32,30 +32,30 @@ public class Style {
     @SuppressWarnings("unchecked")
     public static final Map<String, Map<String, String>> get(
             String name, Function function) throws Exception {
-        long index;
-        Object[] eobjects, aobjects;
-        Map<String, Object> oelement, oattribute;
+        String element;
+        Object[] eobjects;
+        Map<String, Object> oelement;
         Map<String, String> attributes;
-        Iocaste iocaste = new Iocaste(function);
         Map<String, Map<String, String>> elements =
                 new LinkedHashMap<String, Map<String, String>>();
+        Map<String, String[]> ijoin = new LinkedHashMap<String, String[]>();
         
-        eobjects = checkedSelect(iocaste, "SHELL002", "sname = ?", name);
+        ijoin.put("SHELL003", new String[] {"SHELL002.EINDX = SHELL003.EINDX"});
+        eobjects = checkedSelect(new Iocaste(function),
+                "SHELL002", ijoin, "SHELL002.sname = ?", name);
         
         for (Object eobject : eobjects) {
             oelement = (Map<String, Object>)eobject;
-            index = ((BigDecimal)oelement.get("EINDX")).longValue();
-            aobjects = checkedSelect(iocaste, "SHELL003", "eindx = ?", index);
-            
-            attributes = new HashMap<String, String>();
-            
-            for (Object aobject : aobjects) {
-                oattribute = (Map<String, Object>)aobject;
-                attributes.put((String)oattribute.get("PNAME"),
-                        (String)oattribute.get("VALUE"));
+            element = (String)oelement.get("ENAME");
+            if (elements.containsKey(element)) {
+                attributes = elements.get(element);
+            } else {
+                attributes = new HashMap<String, String>();
+                elements.put(element, attributes);
             }
             
-            elements.put((String)oelement.get("ENAME"), attributes);
+            attributes.put((String)oelement.get("PNAME"),
+                    (String)oelement.get("VALUE"));
         }
         
         return elements;
