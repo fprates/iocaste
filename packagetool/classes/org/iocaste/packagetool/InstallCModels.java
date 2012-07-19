b62cfbad8c1b7ca914e553e184d39cfe8afe17e3
Francisco Prates <mad.kanie@gmail.com>
2012-07-19 01:21:49 -0300

    packagetool: transfere criaçao do link documento/documento complexo para create().
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- packagetool/classes/org/iocaste/packagetool/InstallCModels.java
+++ packagetool/classes/org/iocaste/packagetool/InstallCModels.java
@@ -1,13 +1,7 @@
 package org.iocaste.packagetool;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.iocaste.documents.common.ComplexModel;
-import org.iocaste.documents.common.DataElement;
-import org.iocaste.documents.common.DocumentModel;
-import org.iocaste.documents.common.DocumentModelItem;
-import org.iocaste.documents.common.DocumentModelKey;
+import org.iocaste.documents.common.ExtendedObject;
 
 public class InstallCModels {
     
@@ -15,49 +9,19 @@ public class InstallCModels {
      * 
      * @param cmodels
      * @param state
-     * @throws Exception
      */
-    public static final void init(ComplexModel[] cmodels, State state)
-            throws Exception {
+    public static final void init(ComplexModel[] cmodels, State state) {
+        ExtendedObject object;
         String name;
-        DocumentModel model, dmodel;
-        DocumentModelItem item;
-        List<DocumentModel> models = new ArrayList<DocumentModel>();
-        DocumentModelItem ditem, cdocid = state.documents.
-                getModel("COMPLEX_DOCUMENT").getModelItem("ID");
-        DataElement element = cdocid.getDataElement();
         
         for (ComplexModel cmodel : cmodels) {
             state.documents.create(cmodel);
-            Registry.add(cmodel.getName(), "CMODEL", state);
+            name = cmodel.getName();
+            Registry.add(name, "CMODEL", state);
             
-            model = new DocumentModel();
-            model.setName("CMI_"+cmodel.getName());
-            dmodel = cmodel.getHeader();
-            model.setTableName("CMI_"+dmodel.getTableName());
-            
-            item = new DocumentModelItem();
-            item.setName("ID");
-            item.setTableFieldName("IDENT");
-            item.setDataElement(element);
-            item.setReference(cdocid);
-            model.add(item);
-            model.add(new DocumentModelKey(item));
-            
-            for (DocumentModelKey key : dmodel.getKeys()) {
-                name = key.getModelItemName();
-                ditem = dmodel.getModelItem(name);
-                item = new DocumentModelItem();
-                item.setName("Z"+name);
-                item.setTableFieldName("Z"+ditem.getTableFieldName());
-                item.setDataElement(ditem.getDataElement());
-                item.setReference(ditem);
-                model.add(item);
-            }
-            
-            models.add(model);
+            object = state.documents.getObject("COMPLEX_MODEL", name);
+            name = object.getValue("CD_LINK");
+            Registry.add(name, "MODEL", state);
         }
-        
-        InstallModels.init(models.toArray(new DocumentModel[0]), state);
     }
 }
