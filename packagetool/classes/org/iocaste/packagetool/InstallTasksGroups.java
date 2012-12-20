706094f15cf69887b76aab4abd91f62ad98384d6
Francisco Prates <mad.kanie@gmail.com>
2012-12-20 21:37:01 -0200

    packagetool: valida tarefa antes de adicionar à grupo.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- packagetool/classes/org/iocaste/packagetool/InstallTasksGroups.java
+++ packagetool/classes/org/iocaste/packagetool/InstallTasksGroups.java
@@ -5,9 +5,19 @@ import java.util.Set;
 
 import org.iocaste.documents.common.ExtendedObject;
 import org.iocaste.packagetool.common.TaskGroup;
+import org.iocaste.protocol.IocasteException;
 import org.iocaste.protocol.user.User;
 
 public class InstallTasksGroups {
+    
+    private static final boolean containsTask(String name,
+            ExtendedObject[] tasks) {
+        for (ExtendedObject task : tasks)
+            if (task.getValue("NAME").equals(name))
+                return true;
+        
+        return false;
+    }
     
     /**
      * 
@@ -20,7 +30,9 @@ public class InstallTasksGroups {
         String groupname;
         Set<String> itens;
         ExtendedObject task, group;
+        ExtendedObject[] tasks;
         
+        tasks = state.documents.select("from TASKS");
         for (TaskGroup taskgroup : tasksgroups.keySet()) {
             groupname = taskgroup.getName();
             group = TaskSelector.getGroup(groupname, state);
@@ -33,7 +45,13 @@ public class InstallTasksGroups {
             }
 
             itens = taskgroup.getLinks();
+            
             for (String taskname : itens) {
+                if (!containsTask(taskname, tasks))
+                    throw new IocasteException(
+                            new StringBuilder("invalid task \"").
+                            append(taskname).append("\".").toString());
+                
                 task = TaskSelector.addEntry(taskname, group, state);
                 if (state.messages.size() > 0)
                     TaskSelector.addEntryMessage(task, group, state);
