918126d47e2ecbf43d9b55a6ad83c29892114ab7
Francisco Prates <mad.kanie@gmail.com>
2014-01-13 21:35:20 -0200

    shell: associa classe em FileEntryRenderer.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- shell/internal/org/iocaste/internal/renderer/FileEntryRenderer.java
+++ shell/internal/org/iocaste/internal/renderer/FileEntryRenderer.java
@@ -13,10 +13,14 @@ public class FileEntryRenderer extends Renderer {
     public static final XMLElement render(FileEntry file) {
         XMLElement filetag = new XMLElement("input");
         String name = file.getHtmlName();
+        String style = file.getStyleClass();
         
         filetag.add("type", "file");
         filetag.add("name", name);
         filetag.add("id", name);
+        if (style != null)
+            filetag.add("class", style);
+        
         if (!file.isEnabled())
             filetag.add("disabled", "disabled");
         
