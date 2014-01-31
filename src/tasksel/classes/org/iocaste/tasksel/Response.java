17469aeef65fada484804d8b90ed57060a5bf944
Francisco Prates <mad.kanie@gmail.com>
2014-01-31 08:59:36 -0200

    tasksel: tenta centralizar grupos na pagina.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/tasksel/classes/org/iocaste/tasksel/Response.java
+++ src/tasksel/classes/org/iocaste/tasksel/Response.java
@@ -16,6 +16,7 @@ import org.iocaste.shell.common.Link;
 import org.iocaste.shell.common.PageContext;
 import org.iocaste.shell.common.PageControl;
 import org.iocaste.shell.common.Parameter;
+import org.iocaste.shell.common.StandardContainer;
 import org.iocaste.shell.common.StyleSheet;
 import org.iocaste.shell.common.Table;
 import org.iocaste.shell.common.TableColumn;
@@ -99,6 +100,7 @@ public class Response {
      */
     public static final void main(Map<String, Set<TaskEntry>> lists,
             PageContext context) {
+        StandardContainer groups;
         Table table;
         ExpandBar group;
         Link link;
@@ -110,8 +112,16 @@ public class Response {
         PageControl pagecontrol = new PageControl(container);
         
         pagecontrol.add("help", PageControl.EXTERNAL);
+        context.view.setTitle("task-selector");
+        
+        /*
+         * tarefas pré-definidas
+         */
+        if (lists == null)
+            return;
+        
         stylesheet = context.view.styleSheetInstance();
-        stylesheet.clone(".tasksel_eb", ".expand_bar");
+        stylesheet.clone(".tasksel_eb", ".eb_external");
         stylesheet.put(".tasksel_eb", "float", "left");
         stylesheet.put(".tasksel_eb", "display", "inline");
         stylesheet.put(".tasksel_eb", "margin-top", "10px");
@@ -121,34 +131,34 @@ public class Response {
         stylesheet.clone(".tasksel_table_area", ".table_area");
         stylesheet.put(".tasksel_table_area", "border-style", "none");
         
-        /*
-         * tarefas pré-definidas
-         */
-        if (lists != null) {
-            for (String groupname : lists.keySet()) {
-                group = new ExpandBar(container, groupname);
-                group.setExpanded(true);
-                group.setEnabled(false);
-                group.setStyleClass("tasksel_eb");
-                table = new Table(group, groupname.concat(".table"));
-                table.setHeader(false);
-                table.setStyleClass("tasksel_table_area");
-                new TableColumn(table, "link");
-                
-                entries = lists.get(groupname);
-                for (TaskEntry entry : entries) {
-                    groupcommand = new Parameter(container, "groupcommand");
-                    taskname = entry.getName();
-                    text = entry.getText();
-                    link = new Link(table, taskname, "grouprun");
-                    link.setText((text == null)? taskname : text);
-                    link.add(groupcommand, taskname);
-                    new TableItem(table).add(link);
-                }
+        stylesheet.newElement(".tasksel_groups");
+        stylesheet.put(".tasksel_groups", "margin", "auto");
+        stylesheet.put(".tasksel_groups", "background-color", "transparent");
+        stylesheet.put(".tasksel_groups", "width", "90%");
+        
+        groups = new StandardContainer(container, "groups");
+        groups.setStyleClass("tasksel_groups");
+        for (String groupname : lists.keySet()) {
+            group = new ExpandBar(groups, groupname);
+            group.setExpanded(true);
+            group.setEnabled(false);
+            group.setStyleClass("tasksel_eb");
+            table = new Table(group, groupname.concat(".table"));
+            table.setHeader(false);
+            table.setStyleClass("tasksel_table_area");
+            new TableColumn(table, "link");
+            
+            entries = lists.get(groupname);
+            for (TaskEntry entry : entries) {
+                groupcommand = new Parameter(container, "groupcommand");
+                taskname = entry.getName();
+                text = entry.getText();
+                link = new Link(table, taskname, "grouprun");
+                link.setText((text == null)? taskname : text);
+                link.add(groupcommand, taskname);
+                new TableItem(table).add(link);
             }
         }
-        
-        context.view.setTitle("task-selector");
     }
 
 }
