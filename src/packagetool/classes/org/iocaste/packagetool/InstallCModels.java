d23803fe071368b019277191281d8a3752fec348
Francisco Prates <mad.kanie@gmail.com>
2014-04-20 19:03:45 -0300

    packagetool: remove gravaçao de link do documento complexo.
    removemos a geraçao de modelo de link, portanto nao ha necessidade
    de instalar e registrar o mesmo.
    
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/packagetool/classes/org/iocaste/packagetool/InstallCModels.java
+++ src/packagetool/classes/org/iocaste/packagetool/InstallCModels.java
@@ -1,7 +1,6 @@
 package org.iocaste.packagetool;
 
 import org.iocaste.documents.common.ComplexModel;
-import org.iocaste.documents.common.ExtendedObject;
 
 public class InstallCModels {
     
@@ -11,17 +10,12 @@ public class InstallCModels {
      * @param state
      */
     public static final void init(ComplexModel[] cmodels, State state) {
-        ExtendedObject object;
         String name;
         
         for (ComplexModel cmodel : cmodels) {
             state.documents.create(cmodel);
             name = cmodel.getName();
             Registry.add(name, "CMODEL", state);
-            
-            object = state.documents.getObject("COMPLEX_MODEL", name);
-            name = object.get("CD_LINK");
-            Registry.add(name, "MODEL", state);
         }
     }
 }
