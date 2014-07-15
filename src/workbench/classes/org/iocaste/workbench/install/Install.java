322d3ea9dca291d76954de452e9b926f89cf0a52
Francisco Prates <mad.kanie@gmail.com>
2014-07-14 22:02:05 -0300

    workbench: especifica atualizaçao obrigatorio para visao de projeto.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/workbench/classes/org/iocaste/workbench/install/Install.java
+++ src/workbench/classes/org/iocaste/workbench/install/Install.java
@@ -10,56 +10,56 @@ import org.iocaste.protocol.user.UserProfile;
 
 public class Install {
 
-    public static final InstallData execute() {
-        UserProfile profile;
-        TaskGroup taskgroup;
-        Map<String, String> messages;
-        Authorization authorization;
-        InstallData data = new InstallData();
-        
-        messages = new HashMap<>();
-        messages.put("compiler.unavailable", "Compilador indisponível.");
-        messages.put("duplicated.source.name", "Nome de fonte duplicado.");
-        messages.put("invalid.command", "Comando inválido.");
-        messages.put("invalid.package", "Pacote invalido.");
-        messages.put("invalid.package.operation",
-                "Operação inválida para pacote.");
-        messages.put("invalid.project", "Projeto não encontrado.");
-        messages.put("invalid.project.operation",
-                "Operação inválida para projeto.");
-        messages.put("project.removed", "Projeto removido.");
-        messages.put("iocaste-workbench", "Workbench");
-        messages.put("WORKBENCH", "Workbench");
-        messages.put("package.created", "Pacote criado.");
-        messages.put("package.has.no.sources", "Pacote não tem fontes.");
-        messages.put("project.compiled", "Projeto compilado.");
-        messages.put("project.created", "Projeto criado.");
-        messages.put("project.has.no.packages", "Projeto sem pacotes.");
-        messages.put("project.saved", "Projeto salvo.");
-        messages.put("run", "Executar");
-        messages.put("source.created", "Fonte criado.");
-        messages.put("source.modified", "Fonte modificado.");
-        messages.put("source.not.found", "Fonte não encontrado.");
-        messages.put("unspecified.project", "Projeto não especificado.");
-        data.setMessages("pt_BR", messages);
-        
-        authorization = new Authorization("WORKBENCH.EXECUTE");
-        authorization.setObject("APPLICATION");
-        authorization.setAction("EXECUTE");
-        authorization.add("APPNAME", "iocaste-workbench");
-        data.add(authorization);
-        
-        profile = new UserProfile("DEVELOPER");
-        profile.add(authorization);
-        data.add(profile);
-        
-        data.link("WORKBENCH", "iocaste-workbench");
-        taskgroup = new TaskGroup("DEVELOP");
-        taskgroup.add("WORKBENCH");
-        data.add(taskgroup);
-        
-        Models.install(data);
-        
-        return data;
-    }
+//    public static final InstallData execute() {
+//        UserProfile profile;
+//        TaskGroup taskgroup;
+//        Map<String, String> messages;
+//        Authorization authorization;
+//        InstallData data = new InstallData();
+//        
+//        messages = new HashMap<>();
+//        messages.put("compiler.unavailable", "Compilador indisponível.");
+//        messages.put("duplicated.source.name", "Nome de fonte duplicado.");
+//        messages.put("invalid.command", "Comando inválido.");
+//        messages.put("invalid.package", "Pacote invalido.");
+//        messages.put("invalid.package.operation",
+//                "Operação inválida para pacote.");
+//        messages.put("invalid.project", "Projeto não encontrado.");
+//        messages.put("invalid.project.operation",
+//                "Operação inválida para projeto.");
+//        messages.put("project.removed", "Projeto removido.");
+//        messages.put("iocaste-workbench", "Workbench");
+//        messages.put("WORKBENCH", "Workbench");
+//        messages.put("package.created", "Pacote criado.");
+//        messages.put("package.has.no.sources", "Pacote não tem fontes.");
+//        messages.put("project.compiled", "Projeto compilado.");
+//        messages.put("project.created", "Projeto criado.");
+//        messages.put("project.has.no.packages", "Projeto sem pacotes.");
+//        messages.put("project.saved", "Projeto salvo.");
+//        messages.put("run", "Executar");
+//        messages.put("source.created", "Fonte criado.");
+//        messages.put("source.modified", "Fonte modificado.");
+//        messages.put("source.not.found", "Fonte não encontrado.");
+//        messages.put("unspecified.project", "Projeto não especificado.");
+//        data.setMessages("pt_BR", messages);
+//        
+//        authorization = new Authorization("WORKBENCH.EXECUTE");
+//        authorization.setObject("APPLICATION");
+//        authorization.setAction("EXECUTE");
+//        authorization.add("APPNAME", "iocaste-workbench");
+//        data.add(authorization);
+//        
+//        profile = new UserProfile("DEVELOPER");
+//        profile.add(authorization);
+//        data.add(profile);
+//        
+//        data.link("WORKBENCH", "iocaste-workbench");
+//        taskgroup = new TaskGroup("DEVELOP");
+//        taskgroup.add("WORKBENCH");
+//        data.add(taskgroup);
+//        
+//        Models.install(data);
+//        
+//        return data;
+//    }
 }
--- src/workbench/classes/org/iocaste/workbench/install/Models.java
+++ src/workbench/classes/org/iocaste/workbench/install/Models.java
@@ -10,121 +10,121 @@ import org.iocaste.packagetool.common.InstallData;
 
 public class Models {
     public static final void install(InstallData data) {
-        DataElement element, sourceid, sourcename;
-        DocumentModel model;
-        DocumentModelItem item, projectname, packageid;
-        
-        /*
-         * Projeto
-         */
-        model = data.getModel("WB_PROJECT", "WB_PROJECT", null);
-        
-        element = new DataElement("WB_PROJECT_NAME");
-        element.setType(DataType.CHAR);
-        element.setLength(32);
-        element.setUpcase(true);
-        projectname = new DocumentModelItem("PROJECT_NAME");
-        projectname.setTableFieldName("PRJNM");
-        projectname.setDataElement(element);
-        model.add(new DocumentModelKey(projectname));
-        model.add(projectname);
-        
-        element = new DataElement("WB_PROJECT_ID");
-        element.setType(DataType.NUMC);
-        element.setLength(3);
-        item = new DocumentModelItem("PROJECT_ID");
-        item.setTableFieldName("PRJID");
-        item.setDataElement(element);
-        model.add(item);
-        
-        item = new DocumentModelItem("SOURCE_OBJ");
-        item.setTableFieldName("SRCOB");
-        item.setDataElement(new DummyElement("TXTED_TEXTNAME"));
-        model.add(item);
-
-        sourceid = new DataElement("WB_SOURCE_ID");
-        sourceid.setType(DataType.NUMC);
-        sourceid.setLength(9);
-        item = new DocumentModelItem("SOURCE_ID");
-        item.setTableFieldName("SRCID");
-        item.setDataElement(sourceid);
-        model.add(item);
-
-        sourcename = new DataElement("WB_SOURCE_NAME");
-        sourcename.setType(DataType.CHAR);
-        sourcename.setLength(128);
-        sourcename.setUpcase(false);
-        item = new DocumentModelItem("ENTRY_CLASS");
-        item.setTableFieldName("ENTRY");
-        item.setDataElement(sourcename);
-        model.add(item);
-        
-        element = new DataElement("WB_SERVICE_CLASS");
-        element.setType(DataType.CHAR);
-        element.setLength(128);
-        element.setUpcase(false);
-        item = new DocumentModelItem("SERVICE_CLASS");
-        item.setTableFieldName("SRVCL");
-        item.setDataElement(element);
-        model.add(item);
-        
-        data.addNumberFactory("WBPROJECTID");
-        
-        /*
-         * Pacote
-         */
-        model = data.getModel("WB_PACKAGE", "WB_PACKAGE", null);
-        
-        element = new DataElement("WB_PACKAGE_ID");
-        element.setType(DataType.NUMC);
-        element.setLength(6);
-        packageid = new DocumentModelItem("PACKAGE_ID");
-        packageid.setTableFieldName("PKGID");
-        packageid.setDataElement(element);
-        model.add(new DocumentModelKey(packageid));
-        model.add(packageid);
-        
-        item = new DocumentModelItem("PROJECT_NAME");
-        item.setTableFieldName("PRJNM");
-        item.setDataElement(projectname.getDataElement());
-        item.setReference(projectname);
-        model.add(item);
-        
-        element = new DataElement("WB_PACKAGE_NAME");
-        element.setType(DataType.CHAR);
-        element.setLength(128);
-        element.setUpcase(false);
-        item = new DocumentModelItem("PACKAGE_NAME");
-        item.setTableFieldName("PKGNM");
-        item.setDataElement(element);
-        model.add(item);
-        
-        /*
-         * Fonte
-         */
-        model = data.getModel("WB_SOURCE", "WB_SOURCE", null);
-        
-        item = new DocumentModelItem("SOURCE_ID");
-        item.setTableFieldName("SRCID");
-        item.setDataElement(sourceid);
-        model.add(new DocumentModelKey(item));
-        model.add(item);
-        
-        item = new DocumentModelItem("PROJECT_NAME");
-        item.setTableFieldName("PRJNM");
-        item.setDataElement(projectname.getDataElement());
-        item.setReference(projectname);
-        model.add(item);
-
-        item = new DocumentModelItem("PACKAGE_ID");
-        item.setTableFieldName("PKGID");
-        item.setDataElement(packageid.getDataElement());
-        item.setReference(packageid);
-        model.add(item);
-
-        item = new DocumentModelItem("SOURCE_NAME");
-        item.setTableFieldName("SRCNM");
-        item.setDataElement(sourcename);
-        model.add(item);
+//        DataElement element, sourceid, sourcename;
+//        DocumentModel model;
+//        DocumentModelItem item, projectname, packageid;
+//        
+//        /*
+//         * Projeto
+//         */
+//        model = data.getModel("WB_PROJECT", "WB_PROJECT", null);
+//        
+//        element = new DataElement("WB_PROJECT_NAME");
+//        element.setType(DataType.CHAR);
+//        element.setLength(32);
+//        element.setUpcase(true);
+//        projectname = new DocumentModelItem("PROJECT_NAME");
+//        projectname.setTableFieldName("PRJNM");
+//        projectname.setDataElement(element);
+//        model.add(new DocumentModelKey(projectname));
+//        model.add(projectname);
+//        
+//        element = new DataElement("WB_PROJECT_ID");
+//        element.setType(DataType.NUMC);
+//        element.setLength(3);
+//        item = new DocumentModelItem("PROJECT_ID");
+//        item.setTableFieldName("PRJID");
+//        item.setDataElement(element);
+//        model.add(item);
+//        
+//        item = new DocumentModelItem("SOURCE_OBJ");
+//        item.setTableFieldName("SRCOB");
+//        item.setDataElement(new DummyElement("TXTED_TEXTNAME"));
+//        model.add(item);
+//
+//        sourceid = new DataElement("WB_SOURCE_ID");
+//        sourceid.setType(DataType.NUMC);
+//        sourceid.setLength(9);
+//        item = new DocumentModelItem("SOURCE_ID");
+//        item.setTableFieldName("SRCID");
+//        item.setDataElement(sourceid);
+//        model.add(item);
+//
+//        sourcename = new DataElement("WB_SOURCE_NAME");
+//        sourcename.setType(DataType.CHAR);
+//        sourcename.setLength(128);
+//        sourcename.setUpcase(false);
+//        item = new DocumentModelItem("ENTRY_CLASS");
+//        item.setTableFieldName("ENTRY");
+//        item.setDataElement(sourcename);
+//        model.add(item);
+//        
+//        element = new DataElement("WB_SERVICE_CLASS");
+//        element.setType(DataType.CHAR);
+//        element.setLength(128);
+//        element.setUpcase(false);
+//        item = new DocumentModelItem("SERVICE_CLASS");
+//        item.setTableFieldName("SRVCL");
+//        item.setDataElement(element);
+//        model.add(item);
+//        
+//        data.addNumberFactory("WBPROJECTID");
+//        
+//        /*
+//         * Pacote
+//         */
+//        model = data.getModel("WB_PACKAGE", "WB_PACKAGE", null);
+//        
+//        element = new DataElement("WB_PACKAGE_ID");
+//        element.setType(DataType.NUMC);
+//        element.setLength(6);
+//        packageid = new DocumentModelItem("PACKAGE_ID");
+//        packageid.setTableFieldName("PKGID");
+//        packageid.setDataElement(element);
+//        model.add(new DocumentModelKey(packageid));
+//        model.add(packageid);
+//        
+//        item = new DocumentModelItem("PROJECT_NAME");
+//        item.setTableFieldName("PRJNM");
+//        item.setDataElement(projectname.getDataElement());
+//        item.setReference(projectname);
+//        model.add(item);
+//        
+//        element = new DataElement("WB_PACKAGE_NAME");
+//        element.setType(DataType.CHAR);
+//        element.setLength(128);
+//        element.setUpcase(false);
+//        item = new DocumentModelItem("PACKAGE_NAME");
+//        item.setTableFieldName("PKGNM");
+//        item.setDataElement(element);
+//        model.add(item);
+//        
+//        /*
+//         * Fonte
+//         */
+//        model = data.getModel("WB_SOURCE", "WB_SOURCE", null);
+//        
+//        item = new DocumentModelItem("SOURCE_ID");
+//        item.setTableFieldName("SRCID");
+//        item.setDataElement(sourceid);
+//        model.add(new DocumentModelKey(item));
+//        model.add(item);
+//        
+//        item = new DocumentModelItem("PROJECT_NAME");
+//        item.setTableFieldName("PRJNM");
+//        item.setDataElement(projectname.getDataElement());
+//        item.setReference(projectname);
+//        model.add(item);
+//
+//        item = new DocumentModelItem("PACKAGE_ID");
+//        item.setTableFieldName("PKGID");
+//        item.setDataElement(packageid.getDataElement());
+//        item.setReference(packageid);
+//        model.add(item);
+//
+//        item = new DocumentModelItem("SOURCE_NAME");
+//        item.setTableFieldName("SRCNM");
+//        item.setDataElement(sourcename);
+//        model.add(item);
     }
 }
