6e1b7dfea381328435e6ad99f9a5fc304fe88198
Francisco Prates <mad.kanie@gmail.com>
2014-08-09 18:33:27 -0300

    appbuilder: implementa getItem() simplificado.
    Nao e mais necessario usar getTable() seguido de getItem().
    
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/appbuilder/lib/org/iocaste/appbuilder/common/AbstractExtendedValidator.java
+++ src/appbuilder/lib/org/iocaste/appbuilder/common/AbstractExtendedValidator.java
@@ -4,13 +4,50 @@ import org.iocaste.appbuilder.common.tabletool.TableTool;
 import org.iocaste.appbuilder.common.tabletool.TableToolData;
 import org.iocaste.shell.common.AbstractContext;
 import org.iocaste.shell.common.AbstractValidator;
+import org.iocaste.shell.common.Component;
+import org.iocaste.shell.common.Element;
+import org.iocaste.shell.common.InputComponent;
+import org.iocaste.shell.common.Shell;
 import org.iocaste.shell.common.Table;
+import org.iocaste.shell.common.TableItem;
 
 public abstract class AbstractExtendedValidator extends AbstractValidator {
     private static final long serialVersionUID = 8008598121724533805L;
     
     public AbstractExtendedValidator(String name, AbstractContext context) {
         super(name, context);
+    }
+    
+    protected final TableItem getItem(String tablename, InputComponent input) {
+        Element element;
+        Component component;
+        String text;
+        String name = input.getName();
+        Table table = getTable(tablename);
+        
+        for (TableItem item : table.getItems()) {
+            element = item.getElement(name);
+            if (!element.getHtmlName().equals(input.getHtmlName()))
+                continue;
+            
+            if (element.isContainable() || element.isControlComponent())
+                continue;
+            
+            if (element.isDataStorable()) {
+                input = (InputComponent)element;
+                if (Shell.areEquals(input, input.get()))
+                    return item;
+                continue;
+            }
+            
+            component = (Component)element;
+            text = component.getText();
+            if (text.equals(input.get()))
+                return item;
+        }
+        
+        return null;
+        
     }
     
     protected final Table getTable(String name) {
