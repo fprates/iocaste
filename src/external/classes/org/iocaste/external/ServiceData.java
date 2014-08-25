c0277b75e9c96c596a6050fc60b672c877ef42e4
Francisco Prates <mad.kanie@gmail.com>
2014-08-24 21:24:24 -0300

    examples, external, transport: remove módulos inacabados.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/external/classes/org/iocaste/external/ServiceData.java
+++
@@ -1,9 +0,0 @@
-package org.iocaste.external;
-
-import java.util.Map;
-
-public class ServiceData {
-    public Map<String, Port> ports;
-    public Map<String, Type> types;
-    public String name, namespace;
-}
