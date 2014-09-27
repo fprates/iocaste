81df69cbb25d24b5cce28de278157791ab8bc088
Francisco Prates <mad.kanie@gmail.com>
2014-09-27 20:01:15 -0300

    kernel: session nao deveria ser global em set_current_app().
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/kernel/classes/org/iocaste/kernel/session/SetCurrentApp.java
+++ src/kernel/classes/org/iocaste/kernel/session/SetCurrentApp.java
@@ -4,12 +4,12 @@ import org.iocaste.kernel.common.AbstractHandler;
 import org.iocaste.protocol.Message;
 
 public class SetCurrentApp extends AbstractHandler {
-    public Session session;
     
     @Override
     public Object run(Message message) throws Exception {
         String sessionid = message.getSessionid();
         String currentapp = message.getString("current_app");
+        Session session = getFunction();
         
         session.sessions.get(sessionid).setCurrentApp(currentapp);
         return null;
