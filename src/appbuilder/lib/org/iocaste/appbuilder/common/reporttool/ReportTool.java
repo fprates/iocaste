0fc494011c2ffe1967f1b0573f110b2aeb4615ae
Francisco Prates <mad.kanie@gmail.com>
2015-03-09 23:11:28 -0300

    appbuilder: retira limite de linhas em reporttool.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/appbuilder/lib/org/iocaste/appbuilder/common/reporttool/ReportTool.java
+++ src/appbuilder/lib/org/iocaste/appbuilder/common/reporttool/ReportTool.java
@@ -1,21 +1,9 @@
 package org.iocaste.appbuilder.common.reporttool;
 
-import java.util.Locale;
-
 import org.iocaste.appbuilder.common.tabletool.TableTool;
 import org.iocaste.appbuilder.common.tabletool.TableToolData;
-import org.iocaste.documents.common.DataElement;
-import org.iocaste.documents.common.DataType;
-import org.iocaste.documents.common.ExtendedObject;
-import org.iocaste.shell.common.CheckBox;
-import org.iocaste.shell.common.Component;
-import org.iocaste.shell.common.Container;
-import org.iocaste.shell.common.InputComponent;
-import org.iocaste.shell.common.Shell;
 import org.iocaste.shell.common.Table;
 import org.iocaste.shell.common.TableColumn;
-import org.iocaste.shell.common.TableItem;
-import org.iocaste.shell.common.Text;
 
 public class ReportTool {
     private Table table;
@@ -31,51 +19,10 @@ public class ReportTool {
         ttdata.objects = data.objects;
         ttdata.model = data.model;
         ttdata.mode = TableTool.DISPLAY;
+        ttdata.vlines = 0;
         
         new TableTool(ttdata);
     }
-    
-//    public final void setItens(ExtendedObject[] itens) {
-//        DataElement element;
-//        Component component;
-//        InputComponent input;
-//        String stext, name;
-//        TableItem item;
-//        TableColumn[] columns;
-//        Locale locale;
-//        Object value;
-//        
-//        if (itens == null || itens.length == 0)
-//            return;
-//
-//        locale = table.getLocale();
-//        table.importModel(itens[0].getModel());
-//        columns = table.getColumns();
-//        for (ExtendedObject oitem : itens) {
-//            item = new TableItem(table);
-//            for (TableColumn column : columns) {
-//                if (column.isMark())
-//                    continue;
-//
-//                name  = column.getName();
-//                value = oitem.get(name);
-//                element = column.getModelItem().getDataElement();
-//                switch (element.getType()) {
-//                case DataType.BOOLEAN:
-//                    input = new CheckBox(item, name);
-//                    input.set(value);
-//                    input.setEnabled(false);
-//                    break;
-//                default:
-//                    stext = Shell.toString(value, element, locale, false);
-//                    component = new Text(item, name);
-//                    component.setText(stext);
-//                    component.setTranslatable(translate);
-//                    break;
-//                }
-//            }
-//        }
-//    }
     
     public final void setTranslatable(boolean translate) {
         this.translate = translate;
