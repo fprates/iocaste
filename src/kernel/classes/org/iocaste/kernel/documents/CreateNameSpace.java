fbf249422fb8b7b22684e0834df3040a8975de24
Francisco Prates <mad.kanie@gmail.com>
2015-04-06 20:48:31 -0300

    kernel: implementa create_namespace() e remove_namespace().
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/kernel/classes/org/iocaste/kernel/documents/CreateNameSpace.java
+++ src/kernel/classes/org/iocaste/kernel/documents/CreateNameSpace.java
@@ -1,12 +1,47 @@
 package org.iocaste.kernel.documents;
 
+import java.sql.Connection;
+
+import org.iocaste.documents.common.DocumentModel;
+import org.iocaste.documents.common.ExtendedObject;
+import org.iocaste.documents.common.NameSpace;
 import org.iocaste.protocol.Message;
 
 public class CreateNameSpace extends AbstractDocumentsHandler {
 
     @Override
     public Object run(Message message) throws Exception {
-        // TODO Stub de método gerado automaticamente
+        ExtendedObject object;
+        GetDocumentModel getmodel;
+        SaveDocument save;
+        Connection connection;
+        String nsname;
+        int i;
+        DocumentModel model;
+        String sessionid = message.getSessionid();
+        NameSpace ns = message.get("namespace");
+        Documents documents = getFunction();
+        
+        getmodel = documents.get("get_document_model");
+        save = documents.get("save_document");
+        connection = documents.database.getDBConnection(sessionid);
+        nsname = ns.getName();
+        model = getmodel.run(connection, documents, "NS_HEAD");
+        object = new ExtendedObject(model);
+        object.set("NAME", nsname);
+        object.set("MODEL", ns.keymodel);
+        save.run(connection, object);
+        
+        model = getmodel.run(connection, documents, "NS_MODELS");
+        i = 0;
+        for (String name : ns.cmodels) {
+            object = new ExtendedObject(model);
+            object.set("ITEM", String.format("%s%03d", nsname, i));
+            object.set("NAMESPACE", nsname);
+            object.set("COMPLEX_MODEL", name);
+            save.run(connection, object);
+        }
+        
         return null;
     }
 
