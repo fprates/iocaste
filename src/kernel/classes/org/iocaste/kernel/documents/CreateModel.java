07f397626862dca1e1487047a7a99c47096e05cf
Francisco Prates <mad.kanie@gmail.com>
2015-04-10 16:01:18 -0300

    Revert "kernel: cria tabelas com namespace ao inserir entrada chave de namespace."
    This reverts commit d9d90d954511432c617829c669deec3ccbed6483.

--- src/kernel/classes/org/iocaste/kernel/documents/CreateModel.java
+++ src/kernel/classes/org/iocaste/kernel/documents/CreateModel.java
@@ -6,7 +6,6 @@ import org.iocaste.documents.common.DataElement;
 import org.iocaste.documents.common.DocumentModel;
 import org.iocaste.documents.common.DocumentModelItem;
 import org.iocaste.documents.common.DocumentModelKey;
-import org.iocaste.documents.common.NameSpace;
 import org.iocaste.protocol.IocasteException;
 import org.iocaste.protocol.Message;
 
@@ -19,7 +18,7 @@ public class CreateModel extends AbstractDocumentsHandler {
      * @return
      * @throws Exception
      */
-    public final int createTable(Connection connection, Documents documents,
+    private final int createTable(Connection connection, Documents documents,
             DocumentModel model) throws Exception {
         GetDocumentModel getmodel;
         DocumentModel refmodel;
@@ -245,7 +244,9 @@ public class CreateModel extends AbstractDocumentsHandler {
     
     @Override
     public Object run(Message message) throws Exception {
+        int code;
         Documents documents;
+        String name;
         DocumentModel model;
         Connection connection;
         
@@ -253,23 +254,12 @@ public class CreateModel extends AbstractDocumentsHandler {
         documents = getFunction();
         connection = documents.database.getDBConnection(message.getSessionid());
         
-        return run(connection, documents, model, null);
-    }
-    
-    public int run(Connection connection, Documents documents,
-            DocumentModel model, NameSpace ns) throws Exception {
-        int code;
-        String name;
-        
         prepareElements(connection, documents, model);
         if (model.getTableName() != null) {
             code = createTable(connection, documents, model);
             if (code < 0)
                 return code;
         }
-        
-        if (ns != null)
-            return 1;
         
         registerModel(connection, documents, model);        
         name = model.getName();
--- src/kernel/classes/org/iocaste/kernel/documents/SaveDocument.java
+++ src/kernel/classes/org/iocaste/kernel/documents/SaveDocument.java
@@ -2,62 +2,14 @@ package org.iocaste.kernel.documents;
 
 import java.sql.Connection;
 
-import org.iocaste.documents.common.ComplexModel;
 import org.iocaste.documents.common.DocumentModel;
 import org.iocaste.documents.common.DocumentModelItem;
-import org.iocaste.documents.common.DocumentModelKey;
 import org.iocaste.documents.common.ExtendedObject;
 import org.iocaste.documents.common.NameSpace;
-import org.iocaste.protocol.IocasteException;
+import org.iocaste.documents.common.Query;
 import org.iocaste.protocol.Message;
 
 public class SaveDocument extends AbstractDocumentsHandler {
-    
-    private final void createModel(Connection connection, Documents documents,
-            CreateModel createmodel, DocumentModel nsmodel, Object key,
-                    NameSpace ns) throws Exception {
-        String name;
-        
-        name = nsmodel.getTableName();
-        name = new StringBuilder(key.toString()).append(name).toString();
-        nsmodel.setTableName(name);
-        createmodel.run(connection, documents, nsmodel, ns);
-    }
-    
-    private final void createNSModel(Connection connection, Documents documents,
-            DocumentModel model, NameSpace ns, ExtendedObject object)
-                    throws Exception {
-        ComplexModel cmodel;
-        GetComplexModel getcmodel;
-        Object value;
-        CreateModel createmodel;
-        DocumentModel nsmodel;
-
-        value = null;
-        for (DocumentModelKey key : model.getKeys()) {
-            value = object.get(key.getModelItemName());
-            break;
-        }
-
-        if (value == null)
-            throw new IocasteException("undefined key for NS model.");
-        
-        getcmodel = documents.get("get_complex_model");
-        createmodel = documents.get("create_model");
-        for (String cmodelname : ns.cmodels) {
-            cmodel = getcmodel.run(connection, documents, cmodelname);
-            if (cmodel == null)
-                throw new IocasteException(
-                        cmodelname.concat(" is an invalid complex model."));
-            
-            nsmodel = cmodel.getHeader();
-            createModel(connection, documents, createmodel, nsmodel, value, ns);
-            
-            for (DocumentModel imodel : cmodel.getItems().values())
-                createModel(
-                        connection, documents, createmodel, imodel, value, ns);
-        }
-    }
     
     @Override
     public Object run(Message message) throws Exception {
@@ -73,7 +25,7 @@ public class SaveDocument extends AbstractDocumentsHandler {
             Connection connection, Documents documents, ExtendedObject object)
             throws Exception {
         GetDocumentModel getmodel;
-        NameSpace[] nss;
+        NameSpace[] ns;
         DocumentModel model = object.getModel();
         DocumentModelItem[] itens = model.getItens();
         int i = itens.length;
@@ -84,11 +36,10 @@ public class SaveDocument extends AbstractDocumentsHandler {
             criteria[i++] = object.get(item);
         
         getmodel = documents.get("get_document_model");
-        if (model.hasNamespace()) {
-            nss = getmodel.getNS(connection, model);
-            for (NameSpace ns : nss)
-                createNSModel(connection, documents, model, ns, object);
-        }
+        if (model.hasNamespace())
+            ns = getmodel.getNS(connection, model);
+//        if (ns != null)
+//            createNSModel(ns, model);
         
         return update(connection, model.getQuery("insert"), criteria);
     }
