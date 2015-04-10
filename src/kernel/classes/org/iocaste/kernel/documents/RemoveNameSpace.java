2a1068c761bd451d97e8e287fa057b4f8a7b2143
Francisco Prates <mad.kanie@gmail.com>
2015-04-10 16:01:36 -0300

    Revert "kernel: implementa create_namespace() e remove_namespace()."
    This reverts commit fbf249422fb8b7b22684e0834df3040a8975de24.

--- src/kernel/classes/org/iocaste/kernel/documents/RemoveNameSpace.java
+++ src/kernel/classes/org/iocaste/kernel/documents/RemoveNameSpace.java
@@ -1,34 +1,12 @@
 package org.iocaste.kernel.documents;
 
-import java.sql.Connection;
-
-import org.iocaste.documents.common.Query;
 import org.iocaste.protocol.Message;
 
 public class RemoveNameSpace extends AbstractDocumentsHandler {
 
     @Override
     public Object run(Message message) throws Exception {
-        UpdateDocument update;
-        Query query;
-        Connection connection;
-        String sessionid = message.getSessionid();
-        Documents documents = getFunction();
-        String nsname = message.getString("name");
-        
-        update = documents.get("update_document");
-        connection = documents.database.getDBConnection(sessionid);
-        
-        query = new Query("delete");
-        query.setModel("NS_MODELS");
-        query.andEqual("NAMESPACE", nsname);
-        update.run(connection, documents, query);
-        
-        query = new Query("delete");
-        query.setModel("NS_HEAD");
-        query.andEqual("NAME", nsname);
-        update.run(connection, documents, query);
-        
+        // TODO Stub de método gerado automaticamente
         return null;
     }
 
