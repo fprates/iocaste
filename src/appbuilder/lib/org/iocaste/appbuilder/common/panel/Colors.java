f5701a4af183af5336f988e8d806e2524807d8a5
Francisco Prates <mad.kanie@gmail.com>
2015-05-12 20:26:47 -0300

    appbuilder: ajusta contraste de componentes de StandardPanel.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/appbuilder/lib/org/iocaste/appbuilder/common/panel/Colors.java
+++ src/appbuilder/lib/org/iocaste/appbuilder/common/panel/Colors.java
@@ -2,9 +2,9 @@ package org.iocaste.appbuilder.common.panel;
 
 public class Colors {
     public static final String
-            BODY_BG = "#2a2a2a",
+            BODY_BG = "#202020",
             COMPONENT_BG = "#303030",
-            DASH_BG = "#ff0000",
+            DASH_BG = "#5050ff",
             FOCUS = "#505050",
             FONT = "#ffffff";
 }
