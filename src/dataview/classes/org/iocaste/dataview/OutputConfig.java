189323232ae8a50dca14415bea97b24e0704c414
Francisco Prates <mad.kanie@gmail.com>
2015-05-25 12:48:26 -0300

    dataview: remove customização de estilo que impedia ajuste de largura.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/dataview/classes/org/iocaste/dataview/OutputConfig.java
+++ src/dataview/classes/org/iocaste/dataview/OutputConfig.java
@@ -1,7 +1,5 @@
 package org.iocaste.dataview;
 
-import java.util.Map;
-
 import org.iocaste.appbuilder.common.AbstractViewConfig;
 import org.iocaste.appbuilder.common.PageBuilderContext;
 import org.iocaste.appbuilder.common.reporttool.ReportToolData;
@@ -10,18 +8,11 @@ public class OutputConfig extends AbstractViewConfig {
 
     @Override
     protected void execute(PageBuilderContext context) {
-        Map<String, String> style;
         Context extcontext = getExtendedContext();
         ReportToolData reporttool = getReportTool("items");
         
         reporttool.model = extcontext.model.getName();
         getNavControl().setTitle(reporttool.model);
-        
-        style = context.view.styleSheetInstance().get(".std_panel_content");
-        style.put("overflow", "auto");
-        style.put("width", "100%");
-        style.put("height", "100%");
-        style.put("position", "relative");
     }
 
 }
