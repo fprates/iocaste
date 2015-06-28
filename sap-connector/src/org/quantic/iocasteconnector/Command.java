4dc66636e269ef1fe5bef10a580f8a3c8cace1b9
Francisco Prates <mad.kanie@gmail.com>
2015-06-28 16:49:39 -0300

    sapconnector: converte main para utilizar AbstractExternalApplication.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- sap-connector/src/org/quantic/iocasteconnector/Command.java
+++ sap-connector/src/org/quantic/iocasteconnector/Command.java
@@ -1,11 +1,6 @@
 package org.quantic.iocasteconnector;
 
 public class Command {
-    public enum Parameters {
-        NEXT, USER, HOST, LOCALE, PORT, SECRET
-    };
-    
     public int error;
-    public String secret, user, message, host, locale, port;
-    public Parameters type;
+    public String locale, port;
 }
--- sap-connector/src/org/quantic/iocasteconnector/Main.java
+++ sap-connector/src/org/quantic/iocasteconnector/Main.java
@@ -5,8 +5,10 @@ import java.util.Map;
 
 import org.iocaste.documents.common.ComplexDocument;
 import org.iocaste.documents.common.ExtendedObject;
+import org.iocaste.external.common.AbstractExternalApplication;
 import org.iocaste.external.common.External;
 import org.iocaste.external.common.IocasteConnector;
+import org.iocaste.protocol.Message;
 
 import com.sap.conn.jco.JCoException;
 import com.sap.conn.jco.ext.Environment;
@@ -15,9 +17,16 @@ import com.sap.conn.jco.server.JCoServer;
 import com.sap.conn.jco.server.JCoServerFactory;
 import com.sap.conn.jco.server.JCoServerFunctionHandler;
 
-public class Main {
-    
-    public static final void main(String[] args) throws JCoException  {
+public class Main extends AbstractExternalApplication {
+
+	@Override
+	protected void config() {
+		required("--port", KEY_VALUE);
+		option("--language", KEY_VALUE, "pt_BR");
+	}
+
+	@Override
+	protected void execute(Message message) {
         DefaultServerHandlerFactory.FunctionHandlerFactory factory;
         JCoServer server;
         JCoServerFunctionHandler handler;
@@ -25,90 +34,11 @@ public class Main {
         External external;
         IocasteConnector connector;
         ComplexDocument config;
-        Map<String, Command.Parameters> parameters;
-        Command.Parameters waitfor;
         Command stream;
         String name, text;
-        char[] buffer;
         
         stream = new Command();
-        parameters = new HashMap<>();
-        parameters.put("--user", Command.Parameters.USER);
-        parameters.put("--host", Command.Parameters.HOST);
-        parameters.put("--port", Command.Parameters.PORT);
-        parameters.put("--secret", Command.Parameters.SECRET);
-        
-        waitfor = stream.type = Command.Parameters.NEXT;
-        for (String arg : args) {
-            switch(waitfor) {
-            case SECRET:
-                stream.secret = arg;
-                waitfor = Command.Parameters.NEXT;
-                continue;
-            case HOST:
-                stream.host = arg;
-                waitfor = Command.Parameters.NEXT;
-                continue;
-            case USER:
-                stream.user = arg;
-                waitfor = Command.Parameters.NEXT;
-                continue;
-            case LOCALE:
-                stream.locale = arg;
-                waitfor = Command.Parameters.NEXT;
-                continue;
-            case PORT:
-                stream.port = arg;
-                waitfor = Command.Parameters.NEXT;
-                break;
-            default:
-                break;
-            }
-
-            waitfor = parameters.get(arg);
-            if (waitfor == null) {
-                waitfor = Command.Parameters.NEXT;
-                continue;
-            }
-        }
-        
-        if (stream.host == null) {
-            System.err.println("Host not specified.");
-            return;
-        }
-        
-        if (stream.user == null) {
-            System.err.println("Username not specified.");
-            return;
-        }
-        
-        if (stream.port == null) {
-            System.err.println("Connection port not specified.");
-            return;
-        }
-        
-        if (stream.locale == null)
-            stream.locale = "pt_BR";
-        
-        if (stream.secret == null) {
-            System.out.print("Password:");
-            buffer = System.console().readPassword();
-            if (buffer == null) {
-                System.err.println("Password not specified.");
-                return;
-            }
-            stream.secret = new String(buffer);
-        }
-        
-        System.out.print("trying connection to iocaste...");
-        stream.host = "http://".concat(stream.host);
-        connector = new IocasteConnector(stream.host);
-        external = new External(connector);
-        if (!external.connect(stream.user, stream.secret, stream.locale)) {
-            System.err.println("Connection failed.");
-            return;
-        }
-        System.out.println("ok");
+        stream.port = message.getString("--port");
         
         System.out.print("getting connection data from iocaste...");
         config = external.getConnectionData(stream.port);
@@ -146,5 +76,9 @@ public class Main {
 
         System.out.println("listenning to connections...");
         server.start();
-    }
+	}
+	
+	public static final void main(String[] args) {
+		new Main().init(args);
+	}
 }
