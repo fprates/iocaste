d594ccb153f3785669078f1d1560cd6334ba0fac
Francisco Prates <mad.kanie@gmail.com>
2015-09-14 17:15:49 -0300

    external: gera instância do conector.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/external/lib/org/iocaste/external/common/External.java
+++ src/external/lib/org/iocaste/external/common/External.java
@@ -3,8 +3,10 @@ package org.iocaste.external.common;
 import java.util.Map;
 
 import org.iocaste.documents.common.ComplexDocument;
+import org.iocaste.documents.common.ExtendedObject;
 import org.iocaste.protocol.AbstractServiceInterface;
 import org.iocaste.protocol.Function;
+import org.iocaste.protocol.GenericService;
 import org.iocaste.protocol.Message;
 
 public class External extends AbstractServiceInterface {
@@ -34,6 +36,36 @@ public class External extends AbstractServiceInterface {
         return true;
     }
     
+    public final Function connector() {
+        return function;
+    }
+    
+    public static final External connectionInstance(
+            Function function, String port, String language) {
+        Function connector;
+        External external;
+        String user, secret, hostname;
+        ExtendedObject object;
+        ComplexDocument connection;
+        
+        external = new External(function);
+        connection = external.getConnectionData("PRD");
+        if (connection == null)
+            throw new RuntimeException("remote connection config undefined.");
+        
+        object = connection.getHeader();
+        user = object.getst("USERNAME");
+        secret = object.getst("SECRET");
+        hostname = object.getst("HOST");
+        
+        connector = new IocasteConnector(hostname);
+        external = new External(connector);
+        external.setConnection(user, secret, language);
+        if (external.connect())
+            return external;
+        throw new RuntimeException("error connecting to remote iocaste.");
+    }
+    
     public final void disconnect() {
         call(new Message("disconnect"));
     }
@@ -53,6 +85,10 @@ public class External extends AbstractServiceInterface {
         message = new Message("structures_function_get");
         message.add("function", function);
         return call(message);
+    }
+    
+    public final GenericService serviceInstance(String url) {
+        return new GenericService(function, url);
     }
     
     public final void setConnection(String user, String secret, String locale) {
