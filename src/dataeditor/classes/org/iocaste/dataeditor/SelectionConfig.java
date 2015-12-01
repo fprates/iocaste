319734362426797516df2aacc441352e52443c78
Francisco Prates <mad.kanie@gmail.com>
2015-12-01 12:18:41 -0200

    dataeditor: Converte para DataFormTool.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/dataeditor/classes/org/iocaste/dataeditor/SelectionConfig.java
+++ src/dataeditor/classes/org/iocaste/dataeditor/SelectionConfig.java
@@ -2,28 +2,23 @@ package org.iocaste.dataeditor;
 
 import org.iocaste.appbuilder.common.AbstractViewConfig;
 import org.iocaste.appbuilder.common.PageBuilderContext;
-import org.iocaste.shell.common.DataForm;
-import org.iocaste.shell.common.Element;
-import org.iocaste.shell.common.InputComponent;
+import org.iocaste.appbuilder.common.dataformtool.DataFormToolData;
+import org.iocaste.appbuilder.common.dataformtool.DataFormToolItem;
 
 public class SelectionConfig extends AbstractViewConfig {
 
     @Override
     protected void execute(PageBuilderContext context) {
-        InputComponent input;
-        DataForm dataform;
+        DataFormToolData dataform;
+        DataFormToolItem item;
         
         getNavControl().setTitle("dataeditor-selection");
         
-        dataform = getElement("model");
-        dataform.importModel("MODEL", context.function);
-        for (Element element : dataform.getElements())
-            element.setVisible(false);
-        
-        input = dataform.get("NAME");
-        input.setObligatory(true);
-        input.setVisible(true);
-        context.view.setFocus(input);
+        dataform = getDataFormTool("model");
+        dataform.modelname = "MODEL";
+        dataform.show = new String[] {"NAME"};
+        item = dataform.itemInstance("NAME");
+        item.required = item.focus = true;
 
     }
 
