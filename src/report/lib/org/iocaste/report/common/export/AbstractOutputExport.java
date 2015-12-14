057904bd38f183d2a36b28b7cdfb1d8b9c07b0b0
Francisco Prates <mad.kanie@gmail.com>
2015-12-14 12:59:45 -0200

    report: implementa getDataString(), helper para converter data para string.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/report/lib/org/iocaste/report/common/export/AbstractOutputExport.java
+++ src/report/lib/org/iocaste/report/common/export/AbstractOutputExport.java
@@ -1,17 +1,33 @@
 package org.iocaste.report.common.export;
 
+import java.util.Date;
+import java.util.Locale;
 import java.util.Map;
 
 import org.iocaste.appbuilder.common.ExtendedContext;
 import org.iocaste.appbuilder.common.PageBuilderContext;
+import org.iocaste.documents.common.DataElement;
 import org.iocaste.documents.common.ExtendedObject;
+import org.iocaste.shell.common.Shell;
 
 public abstract class AbstractOutputExport {
     private String path[];
     private PageBuilderContext context;
+    private DataElement datede;
+    private Locale locale;
     
     public abstract void formatValues(
             Map<String, String> values, ExtendedObject object);
+
+    protected final String getDateString(ExtendedObject item, String name) {
+        Date date;
+        
+        if (datede == null)
+            datede = item.getModel().getModelItem(name).getDataElement();
+        
+        date = item.getdt(name);
+        return Shell.toString(date, datede, locale, false);
+    }
     
     protected final <T extends ExtendedContext> T getExtendedContext() {
         return context.getView().getExtendedContext();
@@ -23,6 +39,7 @@ public abstract class AbstractOutputExport {
     
     public final void setContext(PageBuilderContext context) {
         this.context = context;
+        locale = context.view.getLocale();
     }
     
     public abstract void setOutputFile(PageBuilderContext context);
