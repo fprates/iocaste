2afb21d2f2ef917dcf80a5b4654ded91e734535e
Francisco Prates <mad.kanie@gmail.com>
2016-01-22 14:07:38 -0200

    upload: atualiza configuração de visão em tela de parâmetros.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/upload/classes/org/iocaste/upload/MainConfig.java
+++ src/upload/classes/org/iocaste/upload/MainConfig.java
@@ -2,30 +2,30 @@ package org.iocaste.upload;
 
 import org.iocaste.appbuilder.common.AbstractViewConfig;
 import org.iocaste.appbuilder.common.PageBuilderContext;
+import org.iocaste.appbuilder.common.dataformtool.DataFormToolData;
+import org.iocaste.appbuilder.common.dataformtool.DataFormToolItem;
 import org.iocaste.shell.common.Const;
-import org.iocaste.shell.common.DataForm;
 import org.iocaste.shell.common.Form;
-import org.iocaste.shell.common.InputComponent;
 
 public class MainConfig extends AbstractViewConfig {
 
     @Override
     public final void execute(PageBuilderContext context) {
-        DataForm dataform;
+        DataFormToolData dataform;
+        DataFormToolItem input;
         Form form;
-        InputComponent input;
         
         form = getElement("main");
         form.setEnctype("multipart/form-data");
         
-        dataform = getElement("options");
-        dataform.importModel("UPL_OPTIONS", context.function);
-        input = dataform.get("FILE");
-        input.setObligatory(true);
-        input.setVisibleLength(64);
-        input.setComponentType(Const.FILE_ENTRY);
+        dataform = getDataFormTool("options");
+        dataform.modelname = "UPL_OPTIONS";
+        input = dataform.itemInstance("FILE");
+        input.required = true;
+        input.vlength = 64;
+        input.componenttype = Const.FILE_ENTRY;
         
-        dataform.get("LAYOUT").setObligatory(true);
-        dataform.get("TRUNCATE_CHAR").setComponentType(Const.CHECKBOX);
+        dataform.itemInstance("LAYOUT").required = true;
+        dataform.itemInstance("TRUNCATE_CHAR").componenttype = Const.CHECKBOX;
     }
 }
