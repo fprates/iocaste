4e54ce49c7210e61332df87533c35793a972751d
Francisco Prates <mad.kanie at gmail.com>
2016-05-21 23:20:53 -0300

    report: AbstractReportContext deve extender AbstractExtendedContext.
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

--- src/report/lib/org/iocaste/report/common/AbstractReportContext.java
+++ src/report/lib/org/iocaste/report/common/AbstractReportContext.java
@@ -3,16 +3,18 @@ package org.iocaste.report.common;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import org.iocaste.appbuilder.common.ExtendedContext;
+import org.iocaste.appbuilder.common.AbstractExtendedContext;
+import org.iocaste.appbuilder.common.PageBuilderContext;
 import org.iocaste.documents.common.ExtendedObject;
 import org.iocaste.report.common.export.AbstractOutputExport;
 
-public abstract class AbstractReportContext implements ExtendedContext {
+public abstract class AbstractReportContext extends AbstractExtendedContext {
     public ExtendedObject object;
     public Collection<ExtendedObject> items;
     public AbstractOutputExport export;
     
-    public AbstractReportContext() {
+    public AbstractReportContext(PageBuilderContext context) {
+        super(context);
         items = new ArrayList<>();
     }
 }
