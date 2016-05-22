d2ebec4137907b773e9c3171479925142efc55b1
Francisco Prates <mad.kanie at gmail.com>
2016-05-22 00:12:25 -0300

    tasksel: remove hardcodes de valores de estilo.
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

--- src/tasksel/classes/org/iocaste/tasksel/Style.java
+++ src/tasksel/classes/org/iocaste/tasksel/Style.java
@@ -3,6 +3,7 @@ package org.iocaste.tasksel;
 import java.util.Map;
 
 import org.iocaste.appbuilder.common.PageBuilderContext;
+import org.iocaste.shell.common.Shell;
 import org.iocaste.shell.common.StyleSheet;
 
 public class Style {
@@ -10,20 +11,24 @@ public class Style {
     public static final void set(PageBuilderContext context) {
         Map<String, String> style;
         StyleSheet stylesheet = context.view.styleSheetInstance();
+        Map<Integer, String> constants = stylesheet.getConstants();
+        String FRAME_COLOR = constants.get(Shell.FRAME_COLOR);
+        String FONT_FAMILY = constants.get(Shell.FONT_FAMILY);
+        String FONT_COLOR = constants.get(Shell.FONT_COLOR);
         
         style = stylesheet.newElement(".tile_frame");
-        style.put("border-top-style", "solid");
-        style.put("border-top-width", "1px");
-        style.put("border-top-color", "rgb(236, 236, 236)");
+        style.put("border-bottom-style", "solid");
+        style.put("border-bottom-width", "1px");
+        style.put("border-bottom-color", FRAME_COLOR);
         
         style = stylesheet.clone(".tile_frame:hover", ".tile_frame");
-        style.put("background-color", "rgb(236, 236, 236)");
+        style.put("background-color", FRAME_COLOR);
         
         style = stylesheet.newElement(".tile_text");
         style.put("margin", "0px");
-        style.put("font-family", "sans-serif");
+        style.put("font-family", FONT_FAMILY);
         style.put("font-size", "12pt");
-        style.put("color", "#444");
+        style.put("color", FONT_COLOR);
         style.put("font-style", "normal");
         style.put("padding", "1em");
         
