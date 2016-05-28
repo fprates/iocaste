00f0f0557e694eb4fc50c45a4805a4d3b014849d
Francisco Prates <mad.kanie at gmail.com>
2016-05-28 13:27:02 -0300

    workbench: valida projeto antes de compilar.
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

--- src/workbench/classes/org/iocaste/workbench/project/compile/Compile.java
+++ src/workbench/classes/org/iocaste/workbench/project/compile/Compile.java
@@ -1,15 +1,19 @@
 package org.iocaste.workbench.project.compile;
 
 import org.iocaste.appbuilder.common.PageBuilderContext;
+import org.iocaste.documents.common.ComplexDocument;
 import org.iocaste.protocol.Iocaste;
 import org.iocaste.protocol.files.Directory;
+import org.iocaste.shell.common.Const;
 import org.iocaste.workbench.AbstractCommand;
 import org.iocaste.workbench.Context;
 
 public class Compile extends AbstractCommand {
+    private CompileData data;
 
     public Compile() {
         required("project");
+        data = new CompileData();
     }
     
     private final void deployApplication(CompileData data)
@@ -26,12 +30,19 @@ public class Compile extends AbstractCommand {
     
     @Override
     protected void execute(PageBuilderContext context) throws Exception {
-        CompileData data = new CompileData();
+        ComplexDocument document;
         
         data.project = parameters.get("project");
+        document = getDocument("project", data.project);
+        if (document == null) {
+            message(Const.ERROR, "invalid.project");
+            return;
+        }
+        
         data.extcontext = getExtendedContext();
         data.context = context;
         deployApplication(data);
+        message(Const.STATUS, "project.compiled");
     }
 
 }
