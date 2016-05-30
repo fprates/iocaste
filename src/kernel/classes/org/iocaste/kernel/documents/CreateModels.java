691a077aff2d059895aba2736c0243d85201f505
Francisco Prates <mad.kanie at gmail.com>
2016-05-30 15:20:20 -0300

    kernel: remove versão anterior antes de criar novo modelo.
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

--- src/kernel/classes/org/iocaste/kernel/documents/CreateModels.java
+++ src/kernel/classes/org/iocaste/kernel/documents/CreateModels.java
@@ -4,10 +4,45 @@ import java.sql.Connection;
 import java.util.Stack;
 
 import org.iocaste.documents.common.DocumentModel;
+import org.iocaste.documents.common.DocumentModelItem;
+import org.iocaste.documents.common.ExtendedObject;
+import org.iocaste.documents.common.Query;
 import org.iocaste.protocol.Message;
 
 public class CreateModels extends AbstractDocumentsHandler {
 
+    private final void recursiveRemove(Connection connection,
+            Documents documents, DocumentModel model) throws Exception {
+        DocumentModel reference;
+        String name = model.getTableName();
+        SelectDocument select = documents.get("select_document");
+        GetDocumentModel getmodel = documents.get("get_document_model");
+        RemoveModel removemodel = documents.get("remove_model");
+        String[] tokens;
+        ExtendedObject[] objects;
+        Query query;
+        String field, modelname;
+        
+        modelname = model.getName();
+        for (DocumentModelItem item : model.getItens()) {
+            field = new StringBuilder(modelname).
+                    append(".").append(item.getName()).toString();
+            query = new Query();
+            query.setModel("FOREIGN_KEY");
+            query.andEqual("ITEM_NAME", field);
+            objects = select.run(connection, query);
+            if (objects == null)
+                continue;
+            for (ExtendedObject object : objects) {
+                tokens = object.getst("REFERENCE").split("\\.");
+                reference = getmodel.run(connection, documents, tokens[0]);
+                recursiveRemove(connection, documents, reference);
+            }
+        }
+        
+        removemodel.removeTable(connection, name);
+    }
+    
     @Override
     public Object run(Message message) throws Exception {
         int code;
@@ -15,6 +50,8 @@ public class CreateModels extends AbstractDocumentsHandler {
         Stack<String> rollback;
         CreateModel createmodel;
         RemoveModel removemodel;
+        GetDocumentModel getmodel;
+        DocumentModel oldmodel;
         DocumentModel[] models = message.get("models");
         Documents documents = getFunction();
         Connection connection = documents.database.
@@ -25,17 +62,21 @@ public class CreateModels extends AbstractDocumentsHandler {
         
         code = 0;
         rollback = new Stack<>();
+        getmodel = documents.get("get_document_model");
         createmodel = documents.get("create_model"); 
         for (DocumentModel model : models) {
             name = model.getTableName();
             if (name == null)
                 continue;
+            oldmodel = getmodel.run(connection, documents, model.getName());
+            if (oldmodel != null)
+                recursiveRemove(connection, documents, oldmodel);
             code = createmodel.createTable(connection, documents, model);
             if (code < 0)
                 break;
             rollback.push(name);
         }
-        
+
         if (code < 0) {
             removemodel = documents.get("remove_model");
             while (!rollback.isEmpty()) {
