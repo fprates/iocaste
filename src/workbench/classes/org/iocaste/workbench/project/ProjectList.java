b3715abffe06b44f57ca6f8c8c4e29f6c92f89c6
Francisco Prates <mad.kanie at gmail.com>
2016-06-22 13:36:22 -0300

    workbench: extende objetos cobertos por project-list.
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

--- src/workbench/classes/org/iocaste/workbench/project/ProjectList.java
+++ src/workbench/classes/org/iocaste/workbench/project/ProjectList.java
@@ -6,16 +6,18 @@ import org.iocaste.documents.common.ExtendedObject;
 import org.iocaste.documents.common.Query;
 import org.iocaste.shell.common.Const;
 import org.iocaste.workbench.AbstractCommand;
-import org.iocaste.workbench.Context;
 
 public class ProjectList extends AbstractCommand {
 
+    public ProjectList() {
+        checkproject = false;
+    }
+    
     @Override
     protected void execute(PageBuilderContext context) throws Exception {
         Query query;
         ExtendedObject[] objects;
         ComplexDocument project;
-        Context extcontext = getExtendedContext();
         
         query = new Query();
         query.setModel("WB_PROJECT_HEAD");
@@ -26,10 +28,20 @@ public class ProjectList extends AbstractCommand {
         }
         for (ExtendedObject object : objects) {
             project = getDocument("project", object.getst("PROJECT_NAME"));
-            extcontext.output.add(project.getstKey());
-            extcontext.output.add("- Telas");
+            print("- Nome: %s", object.getst("PROJECT_NAME"));
+            print("- Perfil: %s", object.getst("PROFILE"));
+            print("- Telas");
             for (ExtendedObject screen : project.getItems("screen"))
-                extcontext.output.add(screen.getst("SCREEN_NAME"));
+                print(screen.getst("SCREEN_NAME"));
+            print("- Links");
+            for (ExtendedObject link : project.getItems("link"))
+                print(link.getst("NAME"));
+            print("- Elementos de dados");
+            for (ExtendedObject dtel : project.getItems("dataelement"))
+                print(dtel.getst("NAME"));
+            print("- Modelos");
+            for (ExtendedObject model : project.getItems("model"))
+                print(model.getst("NAME"));
         }
     }
 }
