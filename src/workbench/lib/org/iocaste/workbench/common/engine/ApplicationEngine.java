0155d97e39c612fef7b33dce834418e01373d790
Francisco Prates <mad.kanie at gmail.com>
2016-06-30 15:05:52 -0300

    workbench: implementa inclusão de ações da visão em engine.
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

--- src/workbench/lib/org/iocaste/workbench/common/engine/ApplicationEngine.java
+++ src/workbench/lib/org/iocaste/workbench/common/engine/ApplicationEngine.java
@@ -1,8 +1,10 @@
 package org.iocaste.workbench.common.engine;
 
+import org.iocaste.appbuilder.common.AbstractActionHandler;
 import org.iocaste.appbuilder.common.AbstractPageBuilder;
 import org.iocaste.appbuilder.common.PageBuilderContext;
 import org.iocaste.appbuilder.common.PageBuilderDefaultInstall;
+import org.iocaste.appbuilder.common.StandardViewInput;
 import org.iocaste.appbuilder.common.panel.AbstractPanelPage;
 import org.iocaste.appbuilder.common.panel.StandardPanel;
 import org.iocaste.protocol.Iocaste;
@@ -13,7 +15,7 @@ import org.iocaste.protocol.utils.XMLConversion;
 public class ApplicationEngine extends AbstractPageBuilder {
 
     @Override
-    public void config(PageBuilderContext context) {
+    public void config(PageBuilderContext context) throws Exception {
         StandardPanel panel;
         Iocaste iocaste;
         byte[] buffer;
@@ -32,7 +34,9 @@ public class ApplicationEngine extends AbstractPageBuilder {
         mapping.add("views.view.config", "item");
         mapping.add("views.view.input", "item");
         mapping.add("views.view.config", "item");
+        mapping.add("views.view.action", "item");
         mapping.setType("views.view.config.item.type", int.class);
+        mapping.setType("views.view.action.item.type", int.class);
         
         extcontext = new Context(context);
         result = conversor.conversion(new String(buffer), mapping);
@@ -41,7 +45,7 @@ public class ApplicationEngine extends AbstractPageBuilder {
         for (ConversionResult view : result.getList("views")) {
             viewname = view.getst("views.view.name");
             extcontext.views.put(viewname, view);
-            panel.instance(viewname, new AutomatedPage(), extcontext);
+            panel.instance(viewname, new AutomatedPage(view), extcontext);
         }
     }
     
@@ -54,11 +58,41 @@ public class ApplicationEngine extends AbstractPageBuilder {
 }
 
 class AutomatedPage extends AbstractPanelPage {
+    private ConversionResult view;
+    
+    public AutomatedPage(ConversionResult view) {
+        this.view = view;
+    }
     
     @Override
-    public void execute() {
+    public void execute() throws Exception {
+        String name, classname;
+        int type;
+        AbstractActionHandler handler;
+        
         set(new AutomatedSpec());
         set(new AutomatedConfig());
+        set(new StandardViewInput());
+        
+        for (ConversionResult action : view.getList("views.view.action")) {
+            name = action.getst("views.view.action.item.name");
+            classname = action.getst("views.view.action.item.class");
+            type = action.geti("views.view.action.item.type");
+            
+            handler = (AbstractActionHandler)Class.forName(classname).
+                    newInstance();
+            switch (type) {
+            case 0:
+                action(name, handler);
+                break;
+            case 1:
+                submit(name, handler);
+                break;
+            case 2:
+                put(name, handler);
+                break;
+            }
+        }
     }
     
 }
