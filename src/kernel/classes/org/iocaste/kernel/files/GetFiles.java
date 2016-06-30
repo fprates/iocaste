d6d12d013b437d44f2f20b93e59d5885f38e7589
Francisco Prates <mad.kanie at gmail.com>
2016-06-30 03:52:17 -0300

    kernel: corrige determinação de File como diretório.
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

--- src/kernel/classes/org/iocaste/kernel/files/GetFiles.java
+++ src/kernel/classes/org/iocaste/kernel/files/GetFiles.java
@@ -10,24 +10,27 @@ public class GetFiles extends AbstractHandler {
 
     @Override
     public Object run(Message message) throws Exception {
-        FileEntry[] files;
-        String[] list;
         String[] args = message.get("args");
+        return run(args);
+    }
+    
+    public FileEntry[] run(String... args) throws Exception {
+        FileEntry[] files;
+        File[] list;
         String path = FileServices.getPath(args);
         File file = new File(path);
         
         if (!file.isDirectory())
             return null;
         
-        list = file.list();
+        list = file.listFiles();
         if ((list == null) || (list.length == 0))
             return null;
         
         files = new FileEntry[list.length];
-        for (int i = 0; i < list.length; i++) {
-            file = new File(list[i]);
-            files[i] = new FileEntry(list[i], args, file.isDirectory());
-        }
+        for (int i = 0; i < list.length; i++)
+            files[i] = new FileEntry(
+                    list[i].getName(), args, list[i].isDirectory());
         
         return files;
     }
