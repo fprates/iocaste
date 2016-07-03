b2864492bb466179530d1b955eada9600b3bf20b
Francisco Prates <mad.kanie at gmail.com>
2016-07-03 00:32:48 -0300

    workbench: configuração de atualização de visão e retorno.
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

--- src/workbench/classes/org/iocaste/workbench/ActionContext.java
+++ src/workbench/classes/org/iocaste/workbench/ActionContext.java
@@ -2,9 +2,12 @@ package org.iocaste.workbench;
 
 import java.util.Map;
 
+import org.iocaste.workbench.project.viewer.ViewerUpdate;
+
 public class ActionContext {
     public Map<String, CommandArgument> arguments;
     public String name;
     public AbstractCommand handler;
-    public boolean mainrestart;
+    public boolean mainrestart, back;
+    public ViewerUpdate updateviewer;
 }
--- src/workbench/classes/org/iocaste/workbench/project/ProjectAdd.java
+++ src/workbench/classes/org/iocaste/workbench/project/ProjectAdd.java
@@ -4,17 +4,22 @@ import org.iocaste.appbuilder.common.PageBuilderContext;
 import org.iocaste.documents.common.ComplexDocument;
 import org.iocaste.shell.common.Const;
 import org.iocaste.workbench.AbstractCommand;
+import org.iocaste.workbench.ActionContext;
 import org.iocaste.workbench.Context;
 
 public class ProjectAdd extends AbstractCommand {
 
     public ProjectAdd(Context extcontext) {
         super("project-add", extcontext);
+        ActionContext actionctx;
+        
         required("name", "PROJECT_NAME");
         required("profile", "PROFILE");
         optional("text", "TEXT");
         checkproject = false;
-        getActionContext().mainrestart = true;
+        actionctx = getActionContext();
+        actionctx.mainrestart = true;
+        actionctx.back = true;
     }
     
     @Override
