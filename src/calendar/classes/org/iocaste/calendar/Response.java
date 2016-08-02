14e40a00be0aca90d4b098e6dd195c9341a8d390
Francisco Prates <mad.kanie at gmail.com>
2016-08-02 09:41:13 -0300

    calendar: ignora perfis de estilo de tables para calendário.
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

--- src/calendar/classes/org/iocaste/calendar/Response.java
+++ src/calendar/classes/org/iocaste/calendar/Response.java
@@ -51,49 +51,9 @@ public class Response {
         Text text;
         Table table;
         TableItem item;
-        StyleSheet stylesheet;
         Locale locale;
-        Map<String, String> style;
-        Map<Integer, String> constants;
         
-        stylesheet = context.view.styleSheetInstance();
-        constants = stylesheet.getConstants();
-        style = stylesheet.newElement(".calcnt");
-        style.put("position", "absolute");
-        style.put("padding", "10px");
-        style.put("float", "left");
-        style.put("overflow", "hidden");
-        style.put("background-color", constants.get(Shell.BACKGROUND_COLOR));
-        style.put("border-style", "solid");
-        style.put("border-width", "1px");
-        style.put("border-color", constants.get(Shell.FRAME_COLOR));
-        style.put("z-index", "1");
-        style.put("margin-top", "2em");
-        style.put("box-shadow", constants.get(Shell.SHADOW));
-        
-        style = stylesheet.clone(".calkey", ".link");
-        style.put("padding", "0px");
-        style.put("margin", "0px");
-        style.put("text-align", "middle");
-        style.put("font-weight", "normal");
-        style.put("display", "block");
-        style = stylesheet.clone(".calkey:hover", ".calkey");
-        
-        style = stylesheet.clone(".caltoday", ".link");
-        style.put("padding", "0px");
-        style.put("margin", "0px");
-        style.put("text-align", "middle");
-        style.put("font-weight", "bold");
-        style.put("display", "block");
-        style = stylesheet.clone(".caltoday:hover", ".caltoday");
-        
-        style = stylesheet.clone(".caldate", ".text");
-        style.put("display", "inline");
-        style.put("text-decoration", "none");
-
-        style = stylesheet.clone(".calmonth", ".caldate");
-        style.put("color", constants.get(Shell.CLICKABLE_COLOR));
-        style.put("cursor", "pointer");
+        Style.execute(context);
         
         context.calendardata.update(context.date);
         container = new StandardContainer(context.view, "calstdcnt");
@@ -114,6 +74,9 @@ public class Response {
         new Parameter(container, "value");
         
         table = new Table(container, "calendar");
+        table.setStyleClass(Table.HEAD, "calthead");
+        table.setStyleClass(Table.TABLE_CELL, "caltd");
+        
         for (String name : context.calendardata.sweekdays)
             new TableColumn(table, name);
         
@@ -167,5 +130,59 @@ public class Response {
 
         new Button(container, "cancel").
                 setEvent("click","closeCal()");
+    }
+}
+
+class Style {
+    public static final void execute(Context context) {
+        StyleSheet stylesheet;
+        Map<String, String> style;
+        Map<Integer, String> constants;
+        
+        stylesheet = context.view.styleSheetInstance();
+        constants = stylesheet.getConstants();
+        style = stylesheet.newElement(".calcnt");
+        style.put("position", "absolute");
+        style.put("padding", "10px");
+        style.put("float", "left");
+        style.put("overflow", "hidden");
+        style.put("background-color", constants.get(Shell.BACKGROUND_COLOR));
+        style.put("border-style", "solid");
+        style.put("border-width", "1px");
+        style.put("border-color", constants.get(Shell.FRAME_COLOR));
+        style.put("z-index", "1");
+        style.put("margin-top", "2em");
+        style.put("box-shadow", constants.get(Shell.SHADOW));
+        
+        style = stylesheet.clone(".calkey", ".link");
+        style.put("padding", "0px");
+        style.put("margin", "0px");
+        style.put("text-align", "middle");
+        style.put("font-weight", "normal");
+        style.put("display", "block");
+        style = stylesheet.clone(".calkey:hover", ".calkey");
+        
+        style = stylesheet.clone(".caltoday", ".link");
+        style.put("padding", "0px");
+        style.put("margin", "0px");
+        style.put("text-align", "middle");
+        style.put("font-weight", "bold");
+        style.put("display", "block");
+        style = stylesheet.clone(".caltoday:hover", ".caltoday");
+        
+        style = stylesheet.clone(".caldate", ".text");
+        style.put("display", "inline");
+        style.put("text-decoration", "none");
+
+        style = stylesheet.clone(".calmonth", ".caldate");
+        style.put("color", constants.get(Shell.CLICKABLE_COLOR));
+        style.put("cursor", "pointer");
+
+        style = stylesheet.clone(".calthead", ".table_head");
+        style.put("display", "table-header-group");
+        
+        style = stylesheet.clone(".caltd", ".table_cell");
+        style.put("display", "table-cell");
+        style.put("float", "unset");
     }
 }
