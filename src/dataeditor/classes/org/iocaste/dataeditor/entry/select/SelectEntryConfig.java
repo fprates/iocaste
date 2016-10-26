b8a1a565959c1b3ca774c0d93c5d7ebf3855385e
Francisco Prates <mad.kanie@gmail.com>
2016-10-26 18:33:15 -0200

    dataeditor: substitui dataform por dataformtooldata em abstractviewconfig.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/dataeditor/classes/org/iocaste/dataeditor/entry/select/SelectEntryConfig.java
+++ src/dataeditor/classes/org/iocaste/dataeditor/entry/select/SelectEntryConfig.java
@@ -2,37 +2,32 @@ package org.iocaste.dataeditor.entry.select;
 
 import org.iocaste.appbuilder.common.AbstractViewConfig;
 import org.iocaste.appbuilder.common.PageBuilderContext;
+import org.iocaste.appbuilder.common.dataformtool.DataFormToolData;
 import org.iocaste.dataeditor.Context;
 import org.iocaste.documents.common.DocumentModel;
+import org.iocaste.documents.common.DocumentModelItem;
 import org.iocaste.documents.common.Documents;
-import org.iocaste.shell.common.DataForm;
-import org.iocaste.shell.common.Element;
-import org.iocaste.shell.common.InputComponent;
 
 public class SelectEntryConfig extends AbstractViewConfig {
 
     @Override
     protected void execute(PageBuilderContext context) {
         Context extcontext;
-        DataForm dataform;
-        InputComponent input;
+        DataFormToolData dataform;
         DocumentModel model;
         
         getNavControl().setTitle("select.entry");
         
         extcontext = getExtendedContext();
+        dataform = getTool("selection");
+        dataform.model = extcontext.model;
+
         model = new Documents(context.function).getModel(extcontext.model);
-        
-        dataform = getElement("selection");
-        DataForm.importModel(dataform, model);
-        
-        for (Element element : dataform.getElements()) {
-            input = (InputComponent)element;
-            if (model.isKey(input.getModelItem())) {
-                input.setObligatory(true);
-                continue;
-            }
-            input.setVisible(false);
+        for (DocumentModelItem item : model.getItens()) {
+            if (model.isKey(item))
+            	dataform.instance(item.getName()).required = true;
+            else
+            	dataform.instance(item.getName()).invisible = true;
         }
     }
 
