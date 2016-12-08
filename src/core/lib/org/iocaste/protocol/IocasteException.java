44e1ad03aaa75f30e1ccfd3e912ff93208a729f8
Francisco Prates <mad.kanie at gmail.com>
2016-12-08 15:50:58 -0200

    core: implementa String.format() em construtor de IocasteException.
    pode substituir StringBuilder e String.concat() em throw new *Exception.
    
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

--- src/core/lib/org/iocaste/protocol/IocasteException.java
+++ src/core/lib/org/iocaste/protocol/IocasteException.java
@@ -3,7 +3,7 @@ package org.iocaste.protocol;
 public class IocasteException extends Exception {
     private static final long serialVersionUID = -3393799052504772427L;
 
-    public IocasteException(String message) {
-        super(message);
+    public IocasteException(String format, Object... args) {
+        super(String.format(format, args));
     }
 }
