433b62928f733dcea6d0e9c116c8dc1f8923d737
Francisco Prates <mad.kanie at gmail.com>
2016-12-10 12:34:49 -0200

    appbuilder: replica namespace do documento para itens das grids.
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

--- src/appbuilder/lib/org/iocaste/appbuilder/common/cmodelviewer/MaintenanceInput.java
+++ src/appbuilder/lib/org/iocaste/appbuilder/common/cmodelviewer/MaintenanceInput.java
@@ -1,13 +1,24 @@
 package org.iocaste.appbuilder.common.cmodelviewer;
 
+import org.iocaste.appbuilder.common.ContextEntry;
 import org.iocaste.appbuilder.common.PageBuilderContext;
 import org.iocaste.appbuilder.common.StandardViewInput;
+import org.iocaste.documents.common.ExtendedObject;
 
 public class MaintenanceInput extends StandardViewInput {
     
     @Override
     protected void execute(PageBuilderContext context) {
+        ContextEntry ctxentry;
+        CModelViewerContext extcontext = getExtendedContext();
+        
         super.execute(context);
+        if (extcontext.ns != null)
+            for (String key : extcontext.models.keySet()) {
+                ctxentry = extcontext.tableInstance(key);
+                for (ExtendedObject object : ctxentry.getObjects())
+                    object.setNS(extcontext.ns);
+            }
         loadInputTexts(context);
     }
 }
