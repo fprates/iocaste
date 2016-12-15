9bcaae93f8f58dc5aa16bb5b2fef3ddcc752dd57
Francisco Prates <mad.kanie at gmail.com>
2016-12-15 11:08:40 -0200

    runtime: implementação inicial de runtime engine.
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

---
+++ src/runtime/lib/org/iocaste/runtime/common/AbstractApplication.java
@@ -0,0 +1,153 @@
+package org.iocaste.runtime.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.iocaste.appbuilder.common.AbstractActionHandler;
+import org.iocaste.appbuilder.common.AbstractExtendedValidator;
+import org.iocaste.appbuilder.common.AbstractInstallObject;
+import org.iocaste.appbuilder.common.BuilderCustomAction;
+import org.iocaste.appbuilder.common.GetFieldsProperties;
+import org.iocaste.appbuilder.common.PageBuilderContext;
+import org.iocaste.appbuilder.common.PageBuilderDefaultInstall;
+import org.iocaste.appbuilder.common.StandardInstallContext;
+import org.iocaste.appbuilder.common.ViewConfig;
+import org.iocaste.appbuilder.common.ViewContext;
+import org.iocaste.appbuilder.common.ViewInput;
+import org.iocaste.appbuilder.common.ViewSpec;
+import org.iocaste.packagetool.common.InstallData;
+import org.iocaste.protocol.GenericService;
+import org.iocaste.protocol.Message;
+import org.iocaste.shell.common.AbstractContext;
+import org.iocaste.shell.common.AbstractPage;
+import org.iocaste.shell.common.InputComponent;
+import org.iocaste.shell.common.View;
+
+public abstract class AbstractApplication extends AbstractPage {
+    private static final String SERVICE = "/iocaste-appbuilder/services.html";
+    private PageBuilderContext context;
+    private StandardInstallContext installcontext;
+    private PageBuilderDefaultInstall defaultinstall;
+    private BuilderCustomAction customaction;
+    private Map<String, AbstractExtendedValidator> validators;
+    
+    public AbstractApplication() {
+        export("install", "install");
+        export("fields_properties_get", new GetFieldsProperties());
+        validators = new HashMap<>();
+    }
+    
+    public abstract void config(PageBuilderContext context) throws Exception;
+    
+    public void config(GetFieldsProperties config) { };
+    
+    protected final void description(String name, String model, String field) {
+        validate(name, new DescriptionValidate(model, field));
+    }
+    
+    /*
+     * (não-Javadoc)
+     * @see org.iocaste.shell.common.AbstractPage#init(
+     *    org.iocaste.shell.common.View)
+     */
+    @Override
+    public AbstractContext init(View view) throws Exception {
+        GenericService service;
+        Message message;
+        Object[] objects;
+        
+        context = new PageBuilderContext();
+        context.view = view;
+        context.function = this;
+        config(context);
+        customaction = new BuilderCustomAction();
+        reassignCustomActions(context);
+        
+        message = new Message("nc_data_get");
+        message.add("style_constants", view.getStyleConstants());
+        service = new GenericService(context.function, SERVICE);
+        objects = service.invoke(message);
+        
+        context.ncsheet = (Object[][])objects[0];
+        context.ncspec = (Object[][])objects[1];
+        context.ncconfig = (Object[][])objects[2];
+        return context;
+    }
+    
+    public final InstallData install(Message message) throws Exception {
+        Map<String, AbstractInstallObject> objects;
+        InstallData data;
+        String pkgname = message.getst("name");
+        
+        installcontext = new StandardInstallContext();
+        defaultinstall = new PageBuilderDefaultInstall(pkgname);
+        installObject("default", defaultinstall);
+        installConfig(defaultinstall);
+        objects = installcontext.getObjects();
+        objects.put("default", defaultinstall);
+        data = installcontext.getInstallData();
+        for (String name : objects.keySet())
+            objects.get(name).run(installcontext);
+        
+        return data;
+    }
+    
+    protected abstract void installConfig(
+            PageBuilderDefaultInstall defaultinstall) throws Exception;
+    
+    protected final void installObject(
+            String name, AbstractInstallObject object) {
+        installcontext.put(name, object);
+    }
+    
+    private final void reassignCustomActions(PageBuilderContext context) {
+        ViewContext viewctx;
+        ViewRuntime customview;
+        ViewSpec viewspec;
+        ViewConfig viewconfig;
+        ViewInput viewinput;
+        
+        for (String name : context.getViews()) {
+            viewctx = context.getView(name);
+            viewspec = viewctx.getSpec();
+            viewconfig = viewctx.getConfig();
+            viewinput = viewctx.getInput();
+            
+            customview = new ViewRuntime();
+            customview.setViewSpec(viewspec);
+            customview.setViewConfig(viewconfig);
+            customview.setViewInput(viewinput);
+            customview.setView(name);
+            
+            register(name, customview);
+            for (String action : viewctx.getActions())
+                register(name, action, viewctx);
+        }
+    }
+    
+    public final void register(String view, String action, ViewContext viewctx)
+    {
+        AbstractActionHandler handler;
+        handler = viewctx.getActionHandler(action);
+        customaction.addHandler(view, action, handler);
+        register(action, customaction);
+    }
+    
+    protected final void validate(
+            String name, AbstractExtendedValidator validator) {
+        validators.put(name, validator);
+    }
+}
+
+class DescriptionValidate extends AbstractExtendedValidator {
+    
+    public DescriptionValidate(String model, String field) {
+        setTextReference(model, field);
+    }
+
+    @Override
+    public void validate() {
+        InputComponent input = getInput();
+        input.setText(getText(input.get()));
+    }
+}
---
+++ src/runtime/lib/org/iocaste/runtime/common/ViewRuntime.java
@@ -0,0 +1,220 @@
+package org.iocaste.runtime.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.iocaste.appbuilder.common.AbstractCustomView;
+import org.iocaste.appbuilder.common.ComponentEntry;
+import org.iocaste.appbuilder.common.ExtendedCustomView;
+import org.iocaste.appbuilder.common.PageBuilderContext;
+import org.iocaste.appbuilder.common.SpecItemHandler;
+import org.iocaste.appbuilder.common.ViewComponents;
+import org.iocaste.appbuilder.common.ViewConfig;
+import org.iocaste.appbuilder.common.ViewContext;
+import org.iocaste.appbuilder.common.ViewInput;
+import org.iocaste.appbuilder.common.ViewSpec;
+import org.iocaste.appbuilder.common.ViewSpecItem;
+import org.iocaste.appbuilder.common.factories.SpecFactory;
+import org.iocaste.appbuilder.common.navcontrol.NavControl;
+import org.iocaste.shell.common.AbstractContext;
+import org.iocaste.shell.common.Media;
+import org.iocaste.shell.common.StyleSheet;
+
+public class ViewRuntime extends AbstractCustomView
+        implements ExtendedCustomView {
+    private Map<ViewSpecItem.TYPES, SpecFactory> factories;
+    private ViewConfig viewconfig;
+    private ViewInput viewinput;
+    private ViewSpec viewspec;
+    
+    public ViewRuntime() {
+        SpecFactory factory;
+        
+        factories = new HashMap<>();
+        for (ViewSpecItem.TYPES type : ViewSpecItem.TYPES.values()) {
+            factory = type.factory();
+            if (factory != null);
+                factories.put(type, factory);
+        }
+    }
+    
+    private void buildItem(PageBuilderContext context,
+            ViewComponents components, ViewSpecItem item, String prefix) {
+        SpecFactory factory;
+        SpecItemHandler handler;
+        
+        factory = getFactory(item);
+        if (factory == null)
+            return;
+        handler = factory.getHandler();
+        if (handler != null)
+            handler.execute(context, item.getName());
+        factory.run(context, components, item, prefix);
+    }
+    
+    private final void download(PageBuilderContext context) throws Exception {
+        context.function.setContentType(
+                context.downloaddata.contenttype);
+        context.function.setContentEncoding(
+                context.downloaddata.contentencoding);
+        context.function.setHeader("Content-Disposition",
+                new StringBuilder("attachment; filename=\"").
+                append(context.downloaddata.filename).
+                append("\"").toString());
+        context.view.setContent(context.downloaddata.content);
+    }
+    
+    @Override
+    public void execute(PageBuilderContext context, ViewSpecItem itemspec,
+            String prefix, boolean processparent) {
+        ComponentEntry entry;
+        SpecFactory factory;
+        PageBuilderContext _context = (PageBuilderContext)context;
+        ViewComponents components = new ViewComponents();
+        
+        viewspec.run(itemspec, _context);
+        for (ViewSpecItem item : viewspec.getItems())
+            if ((itemspec != item) || processparent)
+                buildItem(_context, components, item, prefix);
+        
+        if (viewconfig != null)
+            viewconfig.run(_context, prefix);
+        
+        if (viewinput != null)
+            viewinput.run(_context, true, prefix);
+
+        for (String key : components.entries.keySet()) {
+            entry = components.entries.get(key);
+            factory = factories.get(entry.data.type);
+            if (factory == null)
+                continue;
+            factory.generate(entry, prefix);
+            if (entry.component == null)
+                continue;
+            entry.component.run();
+            entry.component.refresh();
+        }
+        
+        for (ViewSpecItem.TYPES type : ViewSpecItem.TYPES.values()) {
+            factory = factories.get(type);
+            if (factory != null)
+                factory.generate(components);
+        }
+    }
+    
+    /*
+     * (não-Javadoc)
+     * @see org.iocaste.shell.common.CustomView#execute(
+     *    org.iocaste.shell.common.AbstractContext)
+     */
+    @Override
+    public void execute(AbstractContext context) throws Exception {
+        ComponentEntry entry;
+        SpecFactory factory;
+        NavControl navcontrol;
+        ViewContext viewctx;
+        ViewComponents components;
+        PageBuilderContext _context = (PageBuilderContext)context;
+        
+        if (_context.downloaddata != null) {
+            download(_context);
+            _context.downloaddata = null;
+            return;
+        }
+
+        viewctx = _context.getView(getView());
+        components = viewctx.getComponents();
+        if (!viewspec.isInitialized()) {
+            _context.view.clear();
+            viewspec.run(_context);
+            viewctx.reset();
+            
+            for (ViewSpecItem item : viewspec.getItems())
+                buildItem(_context, components, item, null);
+
+            navcontrol = factories.get(ViewSpecItem.TYPES.PAGE_CONTROL).get();
+            if (viewconfig != null) {
+                extendStyleSheet(_context);
+                viewconfig.setNavControl(navcontrol);
+                viewconfig.run(_context);
+            }
+            
+            viewspec.setInitialized(!viewctx.isUpdatable());
+            if (viewinput != null)
+                viewinput.run(_context, true);
+
+            for (String key : components.entries.keySet()) {
+                entry = components.entries.get(key);
+                factory = factories.get(entry.data.type);
+                if (factory == null)
+                    continue;
+                factory.generate(entry);
+                entry.component.run();
+                entry.component.refresh();
+            }
+            
+            for (ViewSpecItem.TYPES type : ViewSpecItem.TYPES.values()) {
+                factory = factories.get(type);
+                if (factory != null)
+                    factory.generate(components);
+            }
+            return;
+        } else {
+            _context.refreshStyle();
+        }
+        
+        if (viewinput != null) {
+            viewinput.run(_context, false);
+            for (String key : components.entries.keySet()) {
+                entry = components.entries.get(key);
+                factory = factories.get(entry.data.type);
+                if (factory == null)
+                    continue;
+                entry.component.refresh();
+            }
+            
+            for (ViewSpecItem item : viewspec.getItems()) {
+                factory = getFactory(item);
+                if (factory != null)
+                    factory.update(components);
+            }
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    private final void extendStyleSheet(PageBuilderContext context) {
+        Map<String, Map<String, String>> sheet;
+        String mediakey;
+        Media media;
+
+        context.stylesheet = StyleSheet.instance(context.view);
+        for (int i = 0; i < context.ncsheet.length; i++) {
+            mediakey = (String)context.ncsheet[i][0];
+            media = context.stylesheet.instanceMedia(mediakey);
+            media.setRule((String)context.ncsheet[i][1]);
+            sheet = (Map<String, Map<String, String>>)context.ncsheet[i][2];
+            context.stylesheet.add(mediakey, sheet);
+        }
+    }
+    
+    private final SpecFactory getFactory(ViewSpecItem item) {
+        ViewSpecItem.TYPES[] types = ViewSpecItem.TYPES.values();
+        
+        return factories.get(types[item.getType()]);
+    }
+    
+    @Override
+    public final void setViewConfig(ViewConfig viewconfig) {
+        this.viewconfig = viewconfig;
+    }
+    
+    @Override
+    public final void setViewInput(ViewInput viewinput) {
+        this.viewinput = viewinput;
+    }
+    
+    @Override
+    public final void setViewSpec(ViewSpec viewspec) {
+        this.viewspec = viewspec;
+    }
+}
