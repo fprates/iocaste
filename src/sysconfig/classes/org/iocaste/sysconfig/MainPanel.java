a7a3eb38f25cbaece79a6de409ecce74b01596ec
Francisco Prates <mad.kanie at gmail.com>
2016-12-28 18:42:12 -0200

    sysconfig: ajusta página antes de tentar carregar itens em grid.
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

--- src/sysconfig/classes/org/iocaste/sysconfig/MainPanel.java
+++ src/sysconfig/classes/org/iocaste/sysconfig/MainPanel.java
@@ -1,17 +1,23 @@
 package org.iocaste.sysconfig;
 
+import org.iocaste.appbuilder.common.ExtendedContext;
 import org.iocaste.appbuilder.common.panel.AbstractPanelPage;
 
 public class MainPanel extends AbstractPanelPage {
 
     @Override
     public void execute() throws Exception {
+        ExtendedContext extcontext;
+        
         set(new MainSpec());
         set(new MainConfig());
         set(new MainInput());
         action("save", new Save());
         put("load", new Load());
-        getExtendedContext().getContext().run("main", "load");
+        
+        extcontext = getExtendedContext();
+        extcontext.pageInstance("main");
+        extcontext.getContext().run("main", "load");
     }
     
 }
