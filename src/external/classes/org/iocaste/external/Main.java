00c57823f4279108105c1712bf819a483b90f7e2
Francisco Prates <mad.kanie at gmail.com>
2017-01-23 15:32:48 -0200

    external: passa a manipular todas as entidades.
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

--- src/external/classes/org/iocaste/external/Main.java
+++ src/external/classes/org/iocaste/external/Main.java
@@ -8,9 +8,14 @@ import org.iocaste.appbuilder.common.GetFieldsProperties;
 import org.iocaste.appbuilder.common.PageBuilderContext;
 import org.iocaste.appbuilder.common.PageBuilderDefaultInstall;
 import org.iocaste.appbuilder.common.cmodelviewer.AbstractModelViewer;
+import org.iocaste.appbuilder.common.cmodelviewer.DisplayMaintenancePage;
+import org.iocaste.appbuilder.common.cmodelviewer.EntityPage;
 import org.iocaste.appbuilder.common.cmodelviewer.Load;
+import org.iocaste.appbuilder.common.cmodelviewer.MaintenancePage;
 import org.iocaste.appbuilder.common.cmodelviewer.Save;
 import org.iocaste.appbuilder.common.cmodelviewer.Validate;
+import org.iocaste.documents.common.ComplexModel;
+import org.iocaste.documents.common.Documents;
 import org.iocaste.external.install.FunctionsInstall;
 import org.iocaste.external.install.PortsInstall;
 
@@ -31,6 +36,8 @@ public class Main extends AbstractModelViewer {
     
     @Override
     public void config(PageBuilderContext context) throws Exception {
+        ComplexModel cmodel;
+        Documents documents;
         AppBuilderLink link;
         
         context.messages = new Messages();
@@ -46,8 +53,22 @@ public class Main extends AbstractModelViewer {
             link.custompage = new ExternalCustomPage();
             link.displaypage = new ExternalDisplayPage();
             link.entitypage = new ExternalEntityPage();
-            
             break;
+        default:
+            link.updateload = new Load(link.edit1view);
+            link.displayload = new Load(link.display1view);
+            link.validate = new Validate();
+            link.save = new Save();
+            link.custompage = new MaintenancePage();
+            link.displaypage = new DisplayMaintenancePage();
+            link.entitypage = new EntityPage();
+            
+            if (link.cmodel != null) {
+                documents = new Documents(context.function);
+                cmodel = documents.getComplexModel(link.cmodel);
+                if (cmodel != null)
+                    link.appname = cmodel.getHeader().getPackage();
+            }
         }
         
         setExtendedContext(new ExternalContext(context));
@@ -68,6 +89,7 @@ public class Main extends AbstractModelViewer {
         link.cmodel = "XTRNL_CONNECTION";
         link.taskgroup = "EXTERNAL";
         link.entity = "externalconn";
+        link.appname = "iocaste-external";
         
         link = defaultinstall.builderLinkInstance();
         link.create = "XTRNLSTRCR";
@@ -85,6 +107,7 @@ public class Main extends AbstractModelViewer {
         link.cmodel = "XTRNL_FUNCTION";
         link.taskgroup = "EXTERNAL";
         link.entity = "externalfunction";
+        link.appname = "iocaste-external";
 
         installObject("functions", new FunctionsInstall());
         installObject("ports", new PortsInstall());
