4825d9a8dd81c219f7a0bb3c6dadd50e1a6b1ef6
Francisco Prates <mad.kanie at gmail.com>
2017-05-27 09:24:27 -0300

    packagetool: inclui installers inacabados, mas desabiitados.
    Signed-off-by: Francisco Prates <mad.kanie at gmail.com>

---
+++ src/packagetool/classes/org/iocaste/packagetool/services/installers/LinkInstaller.java
@@ -0,0 +1,59 @@
+package org.iocaste.packagetool.services.installers;
+
+import org.iocaste.documents.common.ExtendedObject;
+import org.iocaste.packagetool.services.Services;
+import org.iocaste.packagetool.services.State;
+import org.iocaste.protocol.Function;
+
+//public class LinkInstaller extends AbstractModuleInstaller<String, String> {
+public class LinkInstaller {
+
+//    public LinkInstaller(Services services) {
+//        super(services, "TASK");
+//    }
+//
+//    @Override
+//    protected String getObjectName(String key, String object) {
+//        // TODO Auto-generated method stub
+//        return null;
+//    }
+//
+//    @Override
+//    public void install(State state) throws Exception {
+//        // TODO Auto-generated method stub
+//        
+//    }
+//
+//    @Override
+//    protected void install(State state, String key, String object) {
+//        // TODO Auto-generated method stub
+//        
+//    }
+//
+//    @Override
+//    public void remove(ExtendedObject object) {
+//        // TODO Auto-generated method stub
+//        
+//    }
+//
+//    @Override
+//    public void update(State state) throws Exception {
+//        // TODO Auto-generated method stub
+//        
+//    }
+//
+//    @Override
+//    protected void update(State state, String key, String object)
+//            throws Exception {
+//        // TODO Auto-generated method stub
+//        
+//    }
+//
+//    @Override
+//    public void init(Function function) {
+//        // TODO Auto-generated method stub
+//        
+//    }
+    
+}
+
---
+++ src/packagetool/classes/org/iocaste/packagetool/services/installers/TaskGroupItemInstaller.java
@@ -0,0 +1,62 @@
+package org.iocaste.packagetool.services.installers;
+
+import java.util.Set;
+
+import org.iocaste.documents.common.ExtendedObject;
+import org.iocaste.packagetool.common.TaskGroup;
+import org.iocaste.packagetool.services.Services;
+import org.iocaste.packagetool.services.State;
+import org.iocaste.protocol.Function;
+import org.iocaste.protocol.user.User;
+
+public class TaskGroupItemInstaller {
+//        extends AbstractModuleInstaller<TaskGroup, Set<User>> {
+//
+//    public TaskGroupItemInstaller(Services services) {
+//        super(services, "TSKITEM");
+//    }
+//
+//    @Override
+//    protected String getObjectName(TaskGroup key, Set<User> object) {
+//        // TODO Auto-generated method stub
+//        return null;
+//    }
+//
+//    @Override
+//    public void install(State state) throws Exception {
+//        // TODO Auto-generated method stub
+//        
+//    }
+//
+//    @Override
+//    protected void install(State state, TaskGroup key, Set<User> object) {
+//        // TODO Auto-generated method stub
+//        
+//    }
+//
+//    @Override
+//    public void remove(ExtendedObject object) {
+//        // TODO Auto-generated method stub
+//        
+//    }
+//
+//    @Override
+//    public void update(State state) throws Exception {
+//        // TODO Auto-generated method stub
+//        
+//    }
+//
+//    @Override
+//    protected void update(State state, TaskGroup key, Set<User> object)
+//            throws Exception {
+//        // TODO Auto-generated method stub
+//        
+//    }
+//
+//    @Override
+//    public void init(Function function) {
+//        // TODO Auto-generated method stub
+//        
+//    }
+    
+}
