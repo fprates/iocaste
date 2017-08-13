b491d07cb6713bc104c380517f01a2302702cde7
Francisco Prates <mad.kanie@gmail.com>
2017-08-13 11:11:27 -0300

    runtime: tratamento para inicialização de página não definida.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/runtime/lib/org/iocaste/runtime/common/application/AbstractActionHandler.java
+++ src/runtime/lib/org/iocaste/runtime/common/application/AbstractActionHandler.java
@@ -244,11 +244,15 @@ public abstract class AbstractActionHandler<C extends Context>
     }
     
     protected final void init(String view) {
+        ViewSpec spec;
         AbstractPage page = context.getPage(view);
-        ViewSpec spec = page.getSpec();
         
+        if (page == null)
+            throw new IocasteException("page %s undefined.", view);
+        
+        spec = page.getSpec();
         if (spec == null)
-            throw new IocasteException("undefined spec for ", view);
+            throw new IocasteException("undefined spec for page %s.", view);
         spec.setInitialized(false);
     }
     
