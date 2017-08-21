d487b8b7ee05fac03eb4138dc12d933ad4ee53d9
Francisco Prates <mad.kanie@gmail.com>
2017-08-21 11:13:55 -0300

    kernel: ajusta suporte à sessão sem usuário em invalidate_auth_cache().
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/kernel/classes/org/iocaste/kernel/session/InvalidateAuthCache.java
+++ src/kernel/classes/org/iocaste/kernel/session/InvalidateAuthCache.java
@@ -1,5 +1,6 @@
 package org.iocaste.kernel.session;
 
+import org.iocaste.kernel.UserContext;
 import org.iocaste.protocol.AbstractHandler;
 import org.iocaste.protocol.Message;
 
@@ -7,10 +8,14 @@ public class InvalidateAuthCache extends AbstractHandler {
 
     @Override
     public Object run(Message message) throws Exception {
+        UserContext userctx;
         Session session = getFunction();
         
-        for (String sessionid : session.sessions.keySet())
-            session.sessions.get(sessionid).setAuthorizations(null);
+        for (String sessionid : session.sessions.keySet()) {
+            userctx = session.sessions.get(sessionid);
+            if (userctx != null)
+                userctx.setAuthorizations(null);
+        }
         return null;
     }
 
