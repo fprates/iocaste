e97b77921f969a2b6458596dfc07fabf959f92b9
Francisco Prates <mad.kanie@gmail.com>
2017-08-22 12:55:50 -0300

    kernel: corrige atribuição de eventos para itens de tabbedpane.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/kernel/classes/org/iocaste/kernel/runtime/shell/factories/TabbedPaneItemFactory.java
+++ src/kernel/classes/org/iocaste/kernel/runtime/shell/factories/TabbedPaneItemFactory.java
@@ -8,6 +8,13 @@ import org.iocaste.shell.common.TabbedPaneItem;
 public class TabbedPaneItemFactory extends AbstractSpecFactory {
 
     @Override
+    public final void addEventHandler(ViewContext viewctx, String htmlname) {
+        String action;
+        viewctx.addEventHandler(
+                action = htmlname.concat("_bt"), action, "click");
+    }
+    
+    @Override
     protected void execute(ViewContext viewctx,
     		Container container, String parent, String name) {
         new TabbedPaneItem((TabbedPane)container, name);
