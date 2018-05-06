36b60044c8a83f6a96a687b16fb06bca4676c903
Francisco Prates <mad.kanie@gmail.com>
2018-05-06 16:42:46 -0300

    kernel: atribui manipulador de eventos à file_entry.
    Signed-off-by: Francisco Prates <mad.kanie@gmail.com>

--- src/kernel/classes/org/iocaste/kernel/runtime/shell/renderer/legacy/FileEntryRenderer.java
+++ src/kernel/classes/org/iocaste/kernel/runtime/shell/renderer/legacy/FileEntryRenderer.java
@@ -1,11 +1,14 @@
 package org.iocaste.kernel.runtime.shell.renderer.legacy;
 
 import org.iocaste.kernel.runtime.shell.renderer.AbstractElementRenderer;
+import org.iocaste.kernel.runtime.shell.renderer.internal.ActionEventHandler;
 import org.iocaste.kernel.runtime.shell.renderer.internal.Config;
 import org.iocaste.kernel.runtime.shell.renderer.internal.HtmlRenderer;
 import org.iocaste.protocol.utils.XMLElement;
 import org.iocaste.shell.common.Const;
 import org.iocaste.shell.common.InputComponent;
+import org.iocaste.shell.common.Shell;
+import org.iocaste.shell.common.handlers.OnFocusHandler;
 
 public class FileEntryRenderer extends AbstractElementRenderer<InputComponent> {
 
@@ -15,6 +18,7 @@ public class FileEntryRenderer extends AbstractElementRenderer<InputComponent> {
     
     @Override
     protected final XMLElement execute(InputComponent input, Config config) {
+        ActionEventHandler handler;
         XMLElement filetag = new XMLElement("input");
         String name = input.getHtmlName();
         String style = input.getStyleClass();
@@ -27,6 +31,11 @@ public class FileEntryRenderer extends AbstractElementRenderer<InputComponent> {
         
         if (!input.isEnabled())
             filetag.add("disabled", "disabled");
+        
+        handler = config.viewctx.getEventHandler(name, name, "focus");
+        handler.name = name;
+        handler.call = Shell.send(name, "&event=focus");
+        input.put("focus", new OnFocusHandler(input));
         
         return filetag;
     }
